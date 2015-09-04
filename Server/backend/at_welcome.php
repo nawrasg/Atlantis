48b1ed9affff5e17de40a68206cd462197199a0b
Nawras GEORGI <nawrasg@gmail.com>
2015-09-04 16:12:01 +0200

    First commit

---
+++ Server/backend/at_welcome.php
@@ -0,0 +1,41 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+require_once __DIR__.'/classes/connexion.php';
+require_once __DIR__.'/classes/checkAPI.php';
+require_once __DIR__.'/classes/Player.php';
+
+$page_level = 0;
+
+if (isset($_GET['api']) && checkAPI($_GET['api'], $page_level)) {
+    $bdd = getBDD();
+    $req = $bdd->prepare('SELECT * FROM at_welcome WHERE mac = :mac');
+    $req->execute(array('mac' => $_GET['api']));
+    $data = $req->fetch();
+    if($data){
+        $diff = strtotime(now) - strtotime($data['jour']." ".$data['heure']);
+        $diff /= 3600;
+        if($diff > 6){
+            welcome($_GET['api']);
+        }
+    }else{
+        //welcome($_GET['api']);
+    }
+    $req->closeCursor();
+}else{
+    echo 401;
+}
+
+function welcome($api){
+    $bdd = getBDD();
+    $req = $bdd->prepare('UPDATE at_welcome SET jour = now(), heure = now() WHERE mac = :mac');
+    $req->execute(array('mac' => $api));
+    $req->closeCursor();
+    $player = new Player();
+    $req = $bdd->prepare('SELECT at_music.file FROM at_music JOIN at_welcome ON at_music.id = at_welcome.music WHERE at_welcome.mac = mac');
+    $req->execute(array('mac' => $mac));
+    $data = $req->fetch();
+    $player->play($data['file']);
+    $req->closeCursor();
+}
