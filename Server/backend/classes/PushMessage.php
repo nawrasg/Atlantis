534191dfc1461edaf9dcf17b6272b4c9a790c121
Nawras GEORGI <nawrasg@gmail.com>
2015-09-04 15:32:57 +0200

    Add classes for GCM

---
+++ Server/backend/classes/PushMessage.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once __DIR__.'/connexion.php';
+require_once __DIR__.'/GCMPushMessage.php';
+require_once __DIR__.'/Settings.php';
+
+class PushMessage {
+    private $apiKey = "";
+    
+    function __construct(){
+    	$settings = new Settings();
+    	$this->apiKey = $settings->getSettings("Notification", "key");
+    }
+    
+    public function sendMessage($title, $message, $level = 0){
+        $devices = array();
+        $gcpm = new GCMPushMessage($this->apiKey);
+        $bdd = getBDD();
+        $req = $bdd->prepare('SELECT at_gcm.id FROM at_gcm JOIN at_devices ON at_gcm.mac = at_devices.mac JOIN at_users ON at_devices.username = at_users.nom WHERE at_users.type <= :type');
+        $req->execute(array('type' => $level));
+        while ($data = $req->fetch()) {
+            array_push($devices, $data['id']);
+        }
+        $req->closeCursor();
+        $gcpm->setDevices($devices);
+        $response = $gcpm->send($message, array('title' => $title));
+        return $response;
+    }
+    
+    public function sendMessageAll($title, $message){
+        $devices = array();
+        $gcpm = new GCMPushMessage($this->apiKey);
+        $bdd = getBDD();
+        $req = $bdd->query('SELECT id FROM at_gcm');
+        while ($data = $req->fetch()) {
+            array_push($devices, $data['id']);
+        }
+        $req->closeCursor();
+        $gcpm->setDevices($devices);
+        $response = $gcpm->send($message, array('title' => $title));
+        return $response;
+    }
+    
+    public function sendMessageUser($title, $message, $user){
+        
+    }
+}
