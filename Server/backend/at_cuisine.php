93bb3b7997aed16959f1455b7d8556430ee166d7
Nawras GEORGI <nawrasg@gmail.com>
2015-09-14 22:41:27 +0200

    Autoformat

--- Server/backend/at_cuisine.php
+++ Server/backend/at_cuisine.php
@@ -14,7 +14,7 @@ if (isset ( $_REQUEST ['api'] ) && checkAPI ( $_REQUEST ['api'], $page_level )) 
 			echo json_encode ( get () );
 			break;
 		case 'POST' :
-			add($_REQUEST);		
+			add ( $_REQUEST );
 			break;
 		case 'PUT' :
 			if (isset ( $_REQUEST ['ignore'] )) {
@@ -23,11 +23,11 @@ if (isset ( $_REQUEST ['api'] ) && checkAPI ( $_REQUEST ['api'], $page_level )) 
 			if (isset ( $_REQUEST ['open'] )) {
 				open ( $_REQUEST ['open'] );
 			}
-			if(isset($_REQUEST['quantite'], $_REQUEST['id'])){
-				if(isset($_REQUEST['close'])){
-					changeQuantity($_REQUEST['id'], $_REQUEST['quantite'], TRUE);					
-				}else{
-					changeQuantity($_REQUEST['id'], $_REQUEST['quantite']);
+			if (isset ( $_REQUEST ['quantite'], $_REQUEST ['id'] )) {
+				if (isset ( $_REQUEST ['close'] )) {
+					changeQuantity ( $_REQUEST ['id'], $_REQUEST ['quantite'], TRUE );
+				} else {
+					changeQuantity ( $_REQUEST ['id'], $_REQUEST ['quantite'] );
 				}
 			}
 			break;
@@ -38,42 +38,41 @@ if (isset ( $_REQUEST ['api'] ) && checkAPI ( $_REQUEST ['api'], $page_level )) 
 			break;
 	}
 }
-
-function changeQuantity($id, $quantity, $close = FALSE){
-	$bdd = getBDD();
-	if($close){
-		$request = $bdd->exec("UPDATE at_cuisine SET quantite = '$quantity', date2 = 0000-00-00, status = 0 WHERE id = '$id'");
-	}else{
-		$request = $bdd->exec("UPDATE at_cuisine SET quantite = '$quantity' WHERE id = '$id'");
+function changeQuantity($id, $quantity, $close = FALSE) {
+	$bdd = getBDD ();
+	if ($close) {
+		$request = $bdd->exec ( "UPDATE at_cuisine SET quantite = '$quantity', date2 = 0000-00-00, status = 0 WHERE id = '$id'" );
+	} else {
+		$request = $bdd->exec ( "UPDATE at_cuisine SET quantite = '$quantity' WHERE id = '$id'" );
 	}
-	if($request == 1){
-		http_response_code(202);
-	}else{
-		http_response_code(400);
+	if ($request == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
 	}
 }
-
 function ignore($id) {
 	$bdd = getBDD ();
 	$request = $bdd->exec ( "UPDATE at_cuisine SET avoid = 1 WHERE id = '$id'" );
-	if($request == 1){
-		http_response_code(202);
-	}else{
-		http_response_code(400);
+	if ($request == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
 	}
 }
 function open($id) {
 	$bdd = getBDD ();
 	$request = $bdd->exec ( "UPDATE at_cuisine SET status = 1, date2 = now() WHERE id = '$id'" );
-	if($request == 1){
-		http_response_code(202);
-	}else{
-		http_response_code(400);
+	if ($request == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
 	}
 }
 function get() {
 	$bdd = getBDD ();
 	$request = $bdd->query ( 'SELECT * FROM at_cuisine LEFT JOIN at_ean ON at_cuisine.element = at_ean.ean ORDER BY date2 DESC, peremption' );
+	// $request = $bdd->query ( 'SELECT *, SUM(quantite) as quantity FROM (SELECT * FROM at_cuisine LEFT JOIN at_ean ON at_cuisine.element = at_ean.ean ORDER BY date2 DESC, peremption) as tble GROUP BY element' );
 	$today = strtotime ( now );
 	$result = array ();
 	while ( $data = $request->fetch () ) {
@@ -101,16 +100,16 @@ function get() {
 		);
 	}
 	$request->closeCursor ();
-	http_response_code(202);
+	http_response_code ( 202 );
 	return $result;
 }
 function delete($id) {
 	$bdd = getBDD ();
 	$request = $bdd->exec ( "DELETE FROM at_cuisine WHERE id = '$id'" );
-	if($request == 1){
-		http_response_code(202);
-	}else{
-		http_response_code(400);
+	if ($request == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
 	}
 }
 function add($values) {
@@ -127,17 +126,17 @@ function add($values) {
 			$place = 'placard';
 		}
 		$element = $values ['element'];
-		if(isset($values['ean'])){
-			$ean = $values['element'];
-			$label = $values['ean'];
-			$bdd->exec("INSERT INTO at_ean VALUES('$ean', '$label')");
+		if (isset ( $values ['ean'] )) {
+			$ean = $values ['element'];
+			$label = $values ['ean'];
+			$bdd->exec ( "INSERT INTO at_ean VALUES('$ean', '$label')" );
 		}
 		$peremption = $values ['peremption'];
 		$request = $bdd->exec ( "INSERT INTO at_cuisine (`element`, `peremption`, `quantite`, `endroit`) VALUES('$element', '$peremption', '$quantite', '$place')" );
-		if($request == 1){
-			$id = $bdd->lastInsertId();
-			$newRequest = $bdd->query("SELECT * FROM at_cuisine LEFT JOIN at_ean ON at_cuisine.element = at_ean.ean WHERE at_cuisine.id = '$id' ORDER BY date2 DESC, peremption");
-			$data = $newRequest->fetch();
+		if ($request == 1) {
+			$id = $bdd->lastInsertId ();
+			$newRequest = $bdd->query ( "SELECT * FROM at_cuisine LEFT JOIN at_ean ON at_cuisine.element = at_ean.ean WHERE at_cuisine.id = '$id' ORDER BY date2 DESC, peremption" );
+			$data = $newRequest->fetch ();
 			$today = strtotime ( now );
 			$date = strtotime ( $data ['peremption'] ) - $today;
 			$date = intval ( $date / 86400 );
@@ -159,14 +158,14 @@ function add($values) {
 					'endroit' => $data ['endroit'],
 					'status' => $data ['status'],
 					'ouvert' => $date2,
-					'ignore' => $data ['avoid']
+					'ignore' => $data ['avoid'] 
 			);
-			echo json_encode($newItem);
-			http_response_code(202);
-		}else{
-			http_response_code(400);
+			echo json_encode ( $newItem );
+			http_response_code ( 202 );
+		} else {
+			http_response_code ( 400 );
 		}
-	}else{
-		http_response_code(404);
+	} else {
+		http_response_code ( 404 );
 	}
 }
