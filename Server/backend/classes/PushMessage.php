10cc59a949151416f1993878fc11a3952141364f
Nawras GEORGI <nawrasg@gmail.com>
2015-09-16 15:33:03 +0200

    Update field name

--- Server/backend/classes/PushMessage.php
+++ Server/backend/classes/PushMessage.php
@@ -1,56 +1,58 @@
 <?php
-
-require_once __DIR__.'/connexion.php';
-require_once __DIR__.'/GCMPushMessage.php';
-require_once __DIR__.'/Settings.php';
-
+require_once __DIR__ . '/connexion.php';
+require_once __DIR__ . '/GCMPushMessage.php';
+require_once __DIR__ . '/Settings.php';
 class PushMessage {
-    private $apiKey = "";
-    
-    function __construct(){
-    	$settings = new Settings();
-    	$this->apiKey = $settings->getSettings("Notification", "key");
-    }
-    
-    public function sendMessage($title, $message, $level = 0){
-        $devices = array();
-        $gcpm = new GCMPushMessage($this->apiKey);
-        $bdd = getBDD();
-        $req = $bdd->prepare('SELECT at_gcm.id FROM at_gcm JOIN at_devices ON at_gcm.mac = at_devices.mac JOIN at_users ON at_devices.username = at_users.nom WHERE at_users.type <= :type');
-        $req->execute(array('type' => $level));
-        while ($data = $req->fetch()) {
-            array_push($devices, $data['id']);
-        }
-        $req->closeCursor();
-        $gcpm->setDevices($devices);
-        $response = $gcpm->send($message, array('title' => $title));
-        return $response;
-    }
-    
-    public function sendMessageAll($title, $message){
-        $devices = array();
-        $gcpm = new GCMPushMessage($this->apiKey);
-        $bdd = getBDD();
-        $req = $bdd->query('SELECT id FROM at_gcm');
-        while ($data = $req->fetch()) {
-            array_push($devices, $data['id']);
-        }
-        $req->closeCursor();
-        $gcpm->setDevices($devices);
-        $response = $gcpm->send($message, array('title' => $title));
-        return $response;
-    }
-    
-    public function sendMessageDevice($title, $message, $device){
-    	$gcpm = new GCMPushMessage($this->apiKey);
-    	$bdd = getBDD();
-    	$req = $bdd->query("SELECT at_gcm.id FROM at_gcm JOIN at_devices ON at_gcm.mac = at_devices.mac JOIN at_users ON at_devices.username = at_users.id WHERE at_gcm.mac = '$device' LIMIT 1");
-    	$data = $req->fetch();
-    	if(!$data){
-    		return;
-    	}
-    	$gcpm->setDevices($data['id']);
-    	$response = $gcpm->send($message, array('title' => $title));
-    	return $response;
-    }
+	private $apiKey = "";
+	function __construct() {
+		$settings = new Settings ();
+		$this->apiKey = $settings->getSettings ( "Notification", "key" );
+	}
+	public function sendMessage($title, $message, $level = 0) {
+		$devices = array ();
+		$gcpm = new GCMPushMessage ( $this->apiKey );
+		$bdd = getBDD ();
+		$req = $bdd->prepare ( 'SELECT at_gcm.gcm FROM at_gcm JOIN at_devices ON at_gcm.mac = at_devices.mac JOIN at_users ON at_devices.username = at_users.nom WHERE at_users.type <= :type' );
+		$req->execute ( array (
+				'type' => $level 
+		) );
+		while ( $data = $req->fetch () ) {
+			array_push ( $devices, $data ['gcm'] );
+		}
+		$req->closeCursor ();
+		$gcpm->setDevices ( $devices );
+		$response = $gcpm->send ( $message, array (
+				'title' => $title 
+		) );
+		return $response;
+	}
+	public function sendMessageAll($title, $message) {
+		$devices = array ();
+		$gcpm = new GCMPushMessage ( $this->apiKey );
+		$bdd = getBDD ();
+		$req = $bdd->query ( 'SELECT gcm FROM at_gcm' );
+		while ( $data = $req->fetch () ) {
+			array_push ( $devices, $data ['gcm'] );
+		}
+		$req->closeCursor ();
+		$gcpm->setDevices ( $devices );
+		$response = $gcpm->send ( $message, array (
+				'title' => $title 
+		) );
+		return $response;
+	}
+	public function sendMessageDevice($title, $message, $device) {
+		$gcpm = new GCMPushMessage ( $this->apiKey );
+		$bdd = getBDD ();
+		$req = $bdd->query ( "SELECT at_gcm.gcm FROM at_gcm JOIN at_devices ON at_gcm.mac = at_devices.mac JOIN at_users ON at_devices.username = at_users.id WHERE at_gcm.mac = '$device' LIMIT 1" );
+		$data = $req->fetch ();
+		if (! $data) {
+			return;
+		}
+		$gcpm->setDevices ( $data ['gcm'] );
+		$response = $gcpm->send ( $message, array (
+				'title' => $title 
+		) );
+		return $response;
+	}
 }
