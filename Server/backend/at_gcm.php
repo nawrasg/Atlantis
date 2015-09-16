f6dcd273e5b62b03013526cec8e1a4eab5c4d532
Nawras GEORGI <nawrasg@gmail.com>
2015-09-16 15:24:08 +0200

    New structure

--- Server/backend/at_gcm.php
+++ Server/backend/at_gcm.php
@@ -1,32 +1,41 @@
 <?php
 header ( "Access-Control-Allow-Origin: *" );
-require_once './connexion.php';
-require_once './checkAPI.php';
-require_once './at_history_cmd.php';
+header ( 'Access-Control-Allow-Headers: origin, x-requested-with, content-type, accept' );
+header ( 'Access-Control-Allow-Methods: GET, POST, DELETE' );
+
+require_once __DIR__ . '/classes/connexion.php';
+require_once __DIR__ . '/classes/checkAPI.php';
 
 $page_level = 2;
 
-if (isset ( $_GET ['api'] ) && checkAPI ( $_GET ['api'], $page_level )) {
-	if (isset ( $_GET ['action'] )) {
-		$bdd = getBDD ();
-		$req = $bdd->prepare ( 'SELECT * FROM at_gcm WHERE mac = :mac' );
-		$req->execute ( array (
-				'mac' => $_GET ['api'] 
-		) );
-		$resultat = $req->fetch ();
-		if ($resultat) {
-			echo 409;
-		} else {
-			$req2 = $bdd->prepare ( 'INSERT INTO at_gcm VALUES(:mac, :id)' );
-			$req2->execute ( array (
-					'mac' => $_GET ['api'],
-					'id' => $_GET ['action'] 
-			) );
-			echo 200;
-		}
-	} else {
-		echo 404;
+if (isset ( $_REQUEST ['api'] ) && checkAPI ( $_REQUEST ['api'], $page_level )) {
+	switch ($_SERVER ['REQUEST_METHOD']) {
+		case 'POST' :
+			add ( $_REQUEST );
+			break;
+		case 'DELETE' :
+			delete ( $_REQUEST );
+			break;
 	}
-} else {
-	echo 401;
+}
+function add($arr) {
+	$mac = $arr ['api'];
+	$gcm = $arr ['gcm'];
+	$bdd = getBDD ();
+	$req = $bdd->exec ( "INSERT INTO at_gcm VALUES('$mac', '$gcm')" );
+	if ($req == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
+	}
+}
+function delete($arr) {
+	$mac = $arr ['mac'];
+	$bdd = getBDD ();
+	$req = $bdd->exec ( "DELETE FROM at_gcm WHERE mac = '$mac'" );
+	if ($req == 1) {
+		http_response_code ( 202 );
+	} else {
+		http_response_code ( 400 );
+	}
 }
