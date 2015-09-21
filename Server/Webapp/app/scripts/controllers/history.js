e44211ef6fb6624cab2160f8096d2f10acb942fc
Nawras GEORGI <nawrasg@gmail.com>
2015-09-21 23:33:56 +0200

    Add plants moisture+temperature

--- Server/Webapp/app/scripts/controllers/history.js
+++ Server/Webapp/app/scripts/controllers/history.js
@@ -3,15 +3,34 @@
 /**
  * @ngdoc function
  * @name atlantisWebAppApp.controller:HistoryCtrl
- * @description
- * # HistoryCtrl
- * Controller of the atlantisWebAppApp
+ * @description # HistoryCtrl Controller of the atlantisWebAppApp
  */
-angular.module('atlantisWebAppApp')
-  .controller('HistoryCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+nApp.controller('HistoryCtrl', function($scope, $http, $sessionStorage,	$filter, AtlantisUri) {
+	get();
+	$scope.loadPlant = function(){
+		var nURL = AtlantisUri.History() + '?api=' + $sessionStorage.api;
+		nURL += '&plant=' + $scope.plante.id;
+		$http.get(nURL).success(function(data, status){
+			var nLabels = [];
+			var nMoisture = [];
+			var nTemp = [];
+			angular.forEach(data, function(day){
+				nLabels.push($filter('date')(day.date, 'EEEE d'));
+				nMoisture.push($filter('number')(day.moisture_m, 1));
+				nTemp.push($filter('number')(day.soil_temperature_m, 1));
+			});
+			$scope.labels = nLabels;
+			$scope.data = [nMoisture, nTemp];
+			$scope.series = ['Humidité du sol', 'Température du sol'];
+		});
+	};
+	function get() {
+		var nURL = AtlantisUri.History() + '?api=' + $sessionStorage.api;
+		$http.get(nURL).success(function(data, status){
+			if(status == 202){
+				$scope.plantes = data.plants;
+			}
+		});
+	}
+});
