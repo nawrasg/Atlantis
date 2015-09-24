1760d70a5f8ce4083063bea67fb3cf722a0e5c33
Nawras GEORGI <nawrasg@gmail.com>
2015-09-25 00:04:40 +0200

    Add loadSensor() + interval option

--- Server/Webapp/app/scripts/controllers/history.js
+++ Server/Webapp/app/scripts/controllers/history.js
@@ -6,30 +6,57 @@
  * @description # HistoryCtrl Controller of the atlantisWebAppApp
  */
 
-nApp.controller('HistoryCtrl', function($scope, $http, $sessionStorage,	$filter, AtlantisUri) {
+nApp.controller('HistoryCtrl', function($scope, $http, $sessionStorage,
+		$filter, AtlantisUri) {
+	var nChartOptions = {
+		datasetFill : false
+	};
+	$scope.options = nChartOptions;
 	get();
-	$scope.loadPlant = function(){
+	$scope.loadPlant = function() {
 		var nURL = AtlantisUri.History() + '?api=' + $sessionStorage.api;
-		nURL += '&plant=' + $scope.plante.id;
-		$http.get(nURL).success(function(data, status){
+		nURL += '&plant=' + $scope.plante.id + '&interval=' + $scope.interval;
+		$http.get(nURL).success(function(data, status) {
 			var nLabels = [];
 			var nMoisture = [];
 			var nTemp = [];
-			angular.forEach(data, function(day){
+			angular.forEach(data, function(day) {
 				nLabels.push($filter('date')(day.date, 'EEEE d'));
 				nMoisture.push($filter('number')(day.moisture_m, 1));
 				nTemp.push($filter('number')(day.soil_temperature_m, 1));
 			});
 			$scope.labels = nLabels;
-			$scope.data = [nMoisture, nTemp];
-			$scope.series = ['Humidité du sol', 'Température du sol'];
+			$scope.data = [ nMoisture, nTemp ];
+			$scope.series = [ 'Humidité du sol', 'Température du sol' ];
 		});
+	};
+	$scope.loadSensor = function() {
+		var nURL = AtlantisUri.History() + '?api=' + $sessionStorage.api;
+		nURL += '&sensor=' + $scope.capteur.id + '&interval=' + $scope.interval;
+		$http.get(nURL).success(function(data, status) {
+			var nLabels = [];
+			var nValues = [];
+			var nLastDate = '';
+			angular.forEach(data, function(day) {
+				nValues.push($filter('number')(day.value, 1));
+				if(day.date == nLastDate){
+					nLabels.push(day.time);
+				}else{
+					nLabels.push($filter('date')(day.date, 'EEEE d') + ' - ' + day.time);
+					nLastDate = day.date;
+				}
+			});
+			$scope.labels = nLabels;
+			$scope.data = [ nValues ];
+			$scope.series = [ $scope.capteur.type + ' ' + $scope.capteur.unit ];
+		})
 	};
 	function get() {
 		var nURL = AtlantisUri.History() + '?api=' + $sessionStorage.api;
-		$http.get(nURL).success(function(data, status){
-			if(status == 202){
+		$http.get(nURL).success(function(data, status) {
+			if (status == 202) {
 				$scope.plantes = data.plants;
+				$scope.capteurs = data.sensors;
 			}
 		});
 	}
