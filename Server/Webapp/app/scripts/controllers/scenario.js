62d9c8c133ddcdbf447dccb547a71834b7911f70
Nawras GEORGI <nawrasg@gmail.com>
2015-09-30 23:21:03 +0200

    update

--- Server/Webapp/app/scripts/controllers/scenario.js
+++ Server/Webapp/app/scripts/controllers/scenario.js
@@ -6,7 +6,7 @@
  * @description # ScenarioCtrl Controller of the atlantisWebAppApp
  */
 
-nApp.controller('ScenarioCtrl', function($scope, $window, $http, $sessionStorage, AtlantisUri) {
+nApp.controller('ScenarioCtrl', function($scope, $window, $http, $mdToast, $sessionStorage, AtlantisUri) {
 	var blocklyArea = document.getElementById('blocklyArea');
 	var blocklyDiv = document.getElementById('blocklyDiv');
 	var workspace = Blockly.inject(blocklyDiv, {
@@ -30,15 +30,72 @@ nApp.controller('ScenarioCtrl', function($scope, $window, $http, $sessionStorage
 	};
 	$window.addEventListener('resize', onresize, false);
 	onresize();
+	get();
 	$scope.save = function(){
 		var code = Blockly.PHP.workspaceToCode(workspace);
+		var xml = Blockly.Xml.workspaceToDom( Blockly.mainWorkspace );
+		var xml_string = Blockly.Xml.domToText( xml );
 		var data = {
 			api : $sessionStorage.api,
-			name : $scope.scenario,
-			code : code
+			name : $scope.newScenario,
+			code : code,
+			xml : xml_string
 		};
 		$http.put(AtlantisUri.Scenarios(), data).success(function(data, status){
-			console.log(status, data);
+			if(status == 202){
+				showToast($mdToast, 'Scénario enregistré avec succès !');
+				get();
+			}else{
+				showToast($mdToast, 'Un problème est survenu !');
+			}
 		})
 	};
+	$scope.updateTitle = function(){
+		$scope.newScenario = encodeURIComponent($scope.newScenario);
+	};
+	$scope.loadScenario = function(scenario){
+		if(scenario != null){
+			$scope.newScenario = scenario.file;
+			if(scenario.xml !== ''){
+				var xml = Blockly.Xml.textToDom(scenario.xml);
+				Blockly.Xml.domToWorkspace( Blockly.mainWorkspace, xml );			
+			}else{
+				showToast($mdToast, 'Impossible de charger le script !');
+			}			
+		}
+	};
+	$scope.new = function(){
+		$scope.newScenario = '';
+		clear();
+	};
+	$scope.delete = function(){
+		var scenario = $scope.scenario;
+		if(scenario != null && scenario.file != ''){
+			var nURL = AtlantisUri.Scenarios() + '?api=' + $sessionStorage.api + '&scenario=' + scenario.file;
+			$http.delete(nURL).success(function(data, status){
+				if(status == 202){
+					$scope.newScenario = null;
+					$scope.scenario = null;
+					var i = $scope.scenarios.indexOf(scenario);
+					$scope.scenarios.splice(i, 1);
+					clear();
+					showToast($mdToast, 'Scénario effacé avec succès !');
+				}else{
+					showToast($mdToast, 'Un problème est survenu !');
+				}
+			});			
+		}
+	};
+	function clear(){
+		var xml = Blockly.Xml.textToDom('<xml xmlns="http://www.w3.org/1999/xhtml"></xml>');
+		Blockly.Xml.domToWorkspace( Blockly.mainWorkspace, xml );
+	}
+	function get(){
+		var nURL = AtlantisUri.Scenarios() + '?api=' + $sessionStorage.api;
+		$http.get(nURL).success(function(data, status){
+			if(status == 202){
+				$scope.scenarios = data;
+			}
+		});
+	}
 });
