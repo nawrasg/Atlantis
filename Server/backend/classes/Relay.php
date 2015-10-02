ae9f725ec5ec143b9d394fd2de7f247b93ea2608
Nawras GEORGI <nawrasg@gmail.com>
2015-10-02 22:38:21 +0200

    New Relay class

---
+++ Server/backend/classes/Relay.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/connexion.php';
+require_once __DIR__ . '/Zwave.php';
+class Relay {
+	private $switches;
+	public static function byRoom($room) {
+		$switch = new Relay ();
+		$switch->switches = array ();
+		$bdd = getBDD ();
+		$req = $bdd->query ( "SELECT at_sensors.id, at_sensors.sensor, at_sensors.device, at_sensors.protocol, at_sensors_devices.alias, at_sensors_devices.room FROM at_sensors INNER JOIN at_sensors_devices ON at_sensors.device = at_sensors_devices.device WHERE at_sensors.type = 'switchBinary' AND at_sensors.ignore = 0 AND at_sensors_devices.room = $room" );
+		while ( $data = $req->fetch () ) {
+			$switch->switches [] = array (
+					'id' => $data ['id'],
+					'sensor' => $data ['sensor'],
+					'device' => $data ['device'],
+					'protocol' => $data ['protocol'],
+					'alias' => $data ['alias'],
+					'room' => $data ['room'] 
+			);
+		}
+		return $switch;
+	}
+	public function on() {
+		$zwave = new Zwave ();
+		foreach ( $this->switches as $switch ) {
+			switch ($switch ['protocol']) {
+				case 'zwave' :
+					$zwave->command ( $sensor ['sensor'], "'on'" );
+					break;
+			}
+		}
+	}
+	public function off() {
+		$zwave = new Zwave ();
+		foreach ( $this->switches as $switch ) {
+			switch ($switch ['protocol']) {
+				case 'zwave' :
+					$zwave->command ( $sensor ['sensor'], "'off'" );
+					break;
+			}
+		}
+	}
+}
