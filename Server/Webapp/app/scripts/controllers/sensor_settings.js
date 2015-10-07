ec3ddb657f8f0e45615da9007e33c207745c5234
Nawras GEORGI <nawrasg@gmail.com>
2015-10-08 00:07:01 +0200

    Add sensors scenarios

--- Server/Webapp/app/scripts/controllers/sensor_settings.js
+++ Server/Webapp/app/scripts/controllers/sensor_settings.js
@@ -6,8 +6,11 @@
  * @description # SensorSettingsCtrl Controller of the atlantisWebAppApp
  */
 
-nApp.controller('SensorSettingsCtrl', function($scope, $rootScope, $mdDialog, $filter, $sessionStorage, $http, AtlantisUri, sensor) {
+nApp.controller('SensorSettingsCtrl', function($scope, $rootScope, $mdDialog, $filter, $sessionStorage, $http, $mdToast, AtlantisUri, sensor) {
 	$scope.sensor = sensor;
+	if(isScenario()){
+		getScenarios();	
+	}
 	$scope.rooms = $rootScope.rooms;
 	$scope.cancel = function(){
 		$mdDialog.cancel();
@@ -17,6 +20,31 @@ nApp.controller('SensorSettingsCtrl', function($scope, $rootScope, $mdDialog, $f
 	}
 	$scope.delete = function(){
 		
+	};
+	$scope.addScenario = function(scenario){
+		if(scenario != null){
+			var nURL = AtlantisUri.Sensors() + '?api=' + $sessionStorage.api;
+			nURL += '&sensor=' + sensor.id + '&scenario=' + scenario.file;
+			$http.post(nURL).success(function(data, status){
+				if(status == 202){
+					showToast($mdToast, 'Scénario enregistré avec succès !');
+					getScenarios();
+				}else{
+					showToast($mdToast, 'Un problème technique est survenu !');
+				}
+			});
+		}
+	};
+	$scope.removeScenario = function(sc){
+		var nURL = AtlantisUri.Sensors() + '?api=' + $sessionStorage.api + '&scenario=' + sc.id;
+		$http.delete(nURL).success(function(data, status){
+			if(status == 202){
+				var i = $scope.scs.indexOf(sc);
+				$scope.scs.splice(i, 1);
+			}else{
+				showToast($mdToast, 'Un problème technique est survenu !');
+			}
+		});
 	};
 	$scope.save = function(){
 		var nURL = AtlantisUri.Sensors() + "?api=" + $sessionStorage.api;
@@ -33,5 +61,27 @@ nApp.controller('SensorSettingsCtrl', function($scope, $rootScope, $mdDialog, $f
 	};
 	if(sensor.type != 'section'){
 		$scope.lastupdate = $filter('date')(sensor.update * 1000, 'dd/MM/yyyy à hh:mm a');
+	}
+	function getScenarios(){
+		var nURL = AtlantisUri.Scenarios() + '?api=' + $sessionStorage.api;
+		$http.get(nURL).success(function(data, status){
+			if(status == 202){
+				$scope.scenarios = data;				
+			}
+		});
+		var nURL2 = AtlantisUri.Sensors() + '?api=' + $sessionStorage.api + '&scenario=' + sensor.id;
+		$http.get(nURL2).success(function(data, status){
+			if(status == 202){
+				$scope.scs = data;
+			}
+		});
+	}
+	function isScenario(){
+		switch(sensor.type){
+		case 'Door/Window':
+			return true;
+		default:
+			return false;
+		}
 	}
 });
