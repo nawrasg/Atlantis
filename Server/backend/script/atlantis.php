d00328de9d7eaf677e3fb3714f009a6b9011030c
Nawras GEORGI <nawrasg@gmail.com>
2015-10-10 22:53:23 +0200

    Add Motion sensors + scenarios

--- Server/backend/script/atlantis.php
+++ Server/backend/script/atlantis.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/../classes/Zwave.php';
 require_once __DIR__ . '/../classes/PushMessage.php';
 require_once __DIR__ . '/../classes/Settings.php';
 
-// set_time_limit ( 10 );
+$pid = getmypid ();
+(new Settings ())->setSettings ( 'Daemon', 'pid', $pid );
+
 while ( ! getBDD () )
 	;
 
@@ -13,6 +15,7 @@ $zwave = new Zwave ();
 
 $arrMvt = loadSensors ();
 $arrMvt2 = initTimestamp ( $arrMvt );
+$scenarios = loadScenarios ( $arrMvt );
 
 while ( true ) {
 	$alarm = (new Settings ())->getSettings ( 'Alarm', 'status' );
@@ -22,8 +25,21 @@ while ( true ) {
 				case 'zwave' :
 					if ($zwave->getValue ( $sensor ['sensor'] ) == 'on' && $zwave->GetTimestamp ( $sensor ['sensor'] ) != $arrMvt2 [$i]) {
 						$arrMvt2 [$i] = $zwave->GetTimestamp ( $sensor ['sensor'] );
+						execScenario ( $i, $scenarios );
 						if ($alarm) {
 							$push->sendMessageAll ( "Atlantis - Alarme", "Porte ouverte !" );
+						}
+					}
+					break;
+			}
+		} else if ($sensor ['type'] == 'Motion') {
+			switch ($sensor ['protocol']) {
+				case 'zwave' :
+					if ($zwave->GetTimestamp ( $sensor ['sensor'] ) != $arrMvt2 [$i]) {
+						$arrMvt2 [$i] = $zwave->GetTimestamp ( $sensor ['sensor'] );
+						execScenario ( $i, $scenarios );
+						if ($alarm) {
+							$push->sendMessageAll ( "Atlantis - Alarme", "Mouvement !" );
 						}
 					}
 					break;
@@ -35,7 +51,7 @@ while ( true ) {
 function loadSensors() {
 	$arrMvt = array ();
 	$bdd = getBDD ();
-	$req = $bdd->query ( 'SELECT at_sensors.id, at_sensors.sensor, at_sensors.protocol, at_sensors.type, at_sensors_devices.room FROM at_sensors INNER JOIN at_sensors_devices ON `at_sensors`.`device` = `at_sensors_devices`.`device` WHERE `at_sensors`.`ignore` = 0 AND `at_sensors`.`type` = "Door/Window" OR `at_sensors`.`type` = "Temperature"' );
+	$req = $bdd->query ( 'SELECT at_sensors.id, at_sensors.sensor, at_sensors.protocol, at_sensors.type, at_sensors_devices.room FROM at_sensors INNER JOIN at_sensors_devices ON `at_sensors`.`device` = `at_sensors_devices`.`device` WHERE `at_sensors`.`ignore` = 0 AND `at_sensors`.`type` = "Door/Window" OR `at_sensors`.`type` = "Temperature" OR `at_sensors`.`type` = "Motion"' );
 	while ( $data = $req->fetch () ) {
 		array_push ( $arrMvt, $data );
 	}
@@ -49,4 +65,27 @@ function initTimestamp($arr) {
 		array_push ( $arrMvt2, $zwave->GetTimestamp ( $arr [$i] ['sensor'] ) );
 	}
 	return $arrMvt2;
+}
+function loadScenarios($arr) {
+	$bdd = getBDD ();
+	$output = array ();
+	foreach ( $arr as $sensor ) {
+		$result = array ();
+		if ($sensor ['type'] == 'Door/Window' || $sensor ['type'] == 'Motion') {
+			$sensor = $sensor ['id'];
+			$req = $bdd->query ( "SELECT * FROM at_sensors_scenarios WHERE sensor = $sensor" );
+			while ( $data = $req->fetch () ) {
+				array_push ( $result, $data );
+			}
+		}
+		array_push ( $output, $result );
+	}
+	return $output;
+}
+function execScenario($i, $scenarios) {
+	foreach ( $scenarios [$i] as $scenario ) {
+		$name = $scenario ['scenario'];
+		$filename = __DIR__ . "/../scenarios/$name.php";
+		exec ( "nohup php $filename &" );
+	}
 }
