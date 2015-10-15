6e3cd9651b79cd635ac9d23db0eedcd93b4fabcf
Nawras GEORGI <nawrasg@gmail.com>
2015-10-15 22:20:45 +0200

    Implement cameras

--- Server/Webapp/app/scripts/controllers/cameras.js
+++ Server/Webapp/app/scripts/controllers/cameras.js
@@ -3,15 +3,24 @@
 /**
  * @ngdoc function
  * @name atlantisWebAppApp.controller:CamerasCtrl
- * @description
- * # CamerasCtrl
- * Controller of the atlantisWebAppApp
+ * @description # CamerasCtrl Controller of the atlantisWebAppApp
  */
-angular.module('atlantisWebAppApp')
-  .controller('CamerasCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+nApp.controller('CamerasCtrl', function($scope, $http, $sessionStorage, $filter, AtlantisUri) {
+	get();
+	$scope.getTitle = function(camera){
+		if(camera.alias == null || camera.alias == ''){
+			return camera.type;
+		}else{
+			return camera.alias;
+		}
+	};
+	function get(){
+		var nURL = AtlantisUri.Cameras() + '?api=' + $sessionStorage.api;
+		$http.get(nURL).success(function(data, status){
+			if(status == 202){
+				$scope.cameras = data;
+			}
+		})
+	}
+});
