9a27cbece8b55ffcef199e727ca119bb2610f26d
Nawras GEORGI <nawrasg@gmail.com>
2015-10-24 16:29:21 +0200

    Add Alarm class

---
+++ Server/backend/classes/Alarm.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/Settings.php';
+class Alarm {
+	public function isOn() {
+		return (new Settings ())->getSettings ( 'Alarm', 'status' );
+	}
+	public function on() {
+		(new Settings ())->setSettings ( 'Alarm', 'status', true );
+		$this->exec_scenario ();
+	}
+	public function off() {
+		(new Settings ())->setSettings ( 'Alarm', 'status', false );
+		$this->exec_scenario ();
+	}
+	public function toggle() {
+		if ($this->isOn ()) {
+			$this->off ();
+		} else {
+			$this->on ();
+		}
+	}
+	private function exec_scenario() {
+		$scenario = __DIR__ . '/../scenarios/alarm.php';
+		if (file_exists ( $scenario )) {
+			exec ( "nohup php $scenario >/dev/null 2>&1 &" );
+		}
+	}
+}
