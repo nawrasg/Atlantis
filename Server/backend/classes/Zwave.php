f8db40205534dbcf606203caf1936fbb49287d19
Nawras GEORGI <nawrasg@gmail.com>
2015-11-16 19:41:11 +0100

    Add cache + bug fix

--- Server/backend/classes/Zwave.php
+++ Server/backend/classes/Zwave.php
@@ -1,15 +1,19 @@
 <?php
 require_once __DIR__ . '/connexion.php';
-
+require_once __DIR__ . '/Settings.php';
 class Zwave {
 	var $url, $port;
-
-	public function __construct() {
+	var $rawData;
+	public function __construct($local = null) {
 		$settings = new Settings ();
 		$this->url = $settings->getSettings ( "Zwave", "IP" );
 		$this->port = $settings->getSettings ( "Zwave", "Port" );
+		if ($local == null) {
+			$this->rawData = null;
+		} else {
+			$this->rawData = $this->loadRawData ();
+		}
 	}
-
 	private function loadRawData($id = NULL) {
 		// For this to work, file_get_contents requires that allow_url_fopen is enabled
 		if ($id == NULL) {
@@ -25,20 +29,28 @@ class Zwave {
 			return $arr->{'code'};
 		}
 	}
-
 	public function GetTimestamp($id) {
-		$device = $this->loadRawData ( $id );
-		$timestamp = $device->data->updateTime;
-		return $timestamp;
+		if ($this->rawData == null) {
+			$device = $this->loadRawData ( $id );
+			$timestamp = $device->data->updateTime;
+			return $timestamp;
+		} else {
+			$devices = $this->rawData->data->devices;
+			foreach ( $devices as $device ) {
+				if ($device->id == $id) {
+					$timestamp = $device->updateTime;
+					return $timestamp;
+				}
+			}
+			return false;
+		}
 	}
-
 	public function command($id, $cmd) {
 		$link = 'http://' . $this->url . ':' . $this->port . '/ZAutomation/api/v1/devices/' . $id . '/command/' . $cmd;
 		$json = file_get_contents ( $link );
 		$arr = json_decode ( $json );
 		return $arr->{'code'};
 	}
-
 	public function load() {
 		$bdd = getBDD ();
 		$req = $bdd->query ( 'SELECT * FROM at_sensors WHERE `ignore` = 0 AND `protocol` = "zwave" ORDER BY device' );
@@ -66,12 +78,11 @@ class Zwave {
 		$req->closeCursor ();
 		return json_encode ( $arr );
 	}
-	
-	public function loadDevices(){
+	public function loadDevices() {
 		$bdd = getBDD ();
 		$req = $bdd->query ( 'SELECT * FROM at_sensors_devices' );
-		$devices = array();
-		while($data = $req->fetch()){
+		$devices = array ();
+		while ( $data = $req->fetch () ) {
 			$devices [] = array (
 					'id' => $data ['id'],
 					'device' => $data ['device'],
@@ -80,32 +91,34 @@ class Zwave {
 					'type' => 'section' 
 			);
 		}
-		$output = array();
-		for($i = 0; $i < count($devices); $i++){
-			$device = $devices[$i]['device'];
-			$req = $bdd->query("SELECT * FROM at_sensors WHERE `device` = '$device'");
-			$sensors = array();
-			while($data = $req->fetch()){
-				$sensor = $this->loadRawData ( $data['sensor'] );
-				$sensors[] = array(
-						'id' => $data['id'],
-						'sensor' => $data['sensor'],
+		$output = array ();
+		for($i = 0; $i < count ( $devices ); $i ++) {
+			$device = $devices [$i] ['device'];
+			$req = $bdd->query ( "SELECT * FROM at_sensors WHERE `device` = '$device'" );
+			$sensors = array ();
+			while ( $data = $req->fetch () ) {
+				$sensor = $this->loadRawData ( $data ['sensor'] );
+				$sensors [] = array (
+						'id' => $data ['id'],
+						'sensor' => $data ['sensor'],
 						'device' => $device,
-						'type' => $data['type'],
+						'type' => $data ['type'],
 						'unit' => $data ['unit'],
 						'date' => $data ['date'],
 						'time' => $data ['time'],
 						'value' => $sensor->{'data'}->{'metrics'}->{'level'},
 						'update' => $sensor->{'data'}->{'updateTime'},
-						'history' => intval($data ['history']),
-						'ignore' => intval($data ['ignore'])
+						'history' => intval ( $data ['history'] ),
+						'ignore' => intval ( $data ['ignore'] ) 
 				);
 			}
-			$output[] = array('device' => $devices[$i], 'sensors' => $sensors);
+			$output [] = array (
+					'device' => $devices [$i],
+					'sensors' => $sensors 
+			);
 		}
 		return $output;
 	}
-
 	public function discover() {
 		$arr = $this->loadRawData ();
 		$devices = $arr->{'data'}->{'devices'};
@@ -123,12 +136,10 @@ class Zwave {
 		}
 		return 200;
 	}
-
 	private function getRealDeviceName($device) {
 		$arr = explode ( '-', $device );
 		return $arr [0];
 	}
-
 	private function insertDevice($sensor, $device, $type, $unit) {
 		$bdd = getBDD ();
 		$req = $bdd->prepare ( 'INSERT INTO at_sensors VALUES("", :sensor, :device, "zwave", :type, :unit, 0, now(), now(), 0)' );
@@ -146,13 +157,22 @@ class Zwave {
 		$req->closeCursor ();
 		return 200;
 	}
-
 	public function getValue($id) {
-		$sensor = $this->loadRawData ( $id );
-		$value = $sensor->{'data'}->{'metrics'}->{'level'};
-		return $value;
+		if ($this->rawData == null) {
+			$sensor = $this->loadRawData ( $id );
+			$value = $sensor->{'data'}->{'metrics'}->{'level'};
+			return $value;
+		} else {
+			$devices = $this->rawData->data->devices;
+			foreach ( $devices as $device ) {
+				if ($device->id == $id) {
+					$value = $device->metrics->level;
+					return $value;
+				}
+			}
+			return false;
+		}
 	}
-
 	public function delete() {
 		$bdd = getBDD ();
 		$req = $bdd->exec ( 'TRUNCATE TABLE at_sensors' );
@@ -161,14 +181,12 @@ class Zwave {
 		$req2->closeCursor ();
 		return 200;
 	}
-
 	public function inclusion($start) {
 		$status = ($start ? 1 : 0);
 		$link = 'http://' . $this->url . ':' . $this->port . '/ZWaveAPI/Run/controller.AddNodeToNetwork(' . $status . ')';
 		$result = file_get_contents ( $link );
 		return $result;
 	}
-
 	public function exclusion($start) {
 		$status = ($start ? 1 : 0);
 		$link = 'http://' . $this->url . ':' . $this->port . '/ZWaveAPI/Run/controller.RemoveNodeFromNetwork(' . $status . ')';
