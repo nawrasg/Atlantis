6995291e2ed1160367545b48096f2d71899da933
Nawras GEORGI <nawrasg@gmail.com>
2015-12-03 21:43:11 +0100

    First commit

---
+++ Android/src/fr/nawrasg/atlantis/preferences/GeoPreferenceFragment.java
@@ -0,0 +1,18 @@
+package fr.nawrasg.atlantis.preferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import fr.nawrasg.atlantis.R;
+
+/**
+ * Created by Nawras GEORGI on 03/12/2015.
+ */
+public class GeoPreferenceFragment extends PreferenceFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.pref_geo);
+	}
+}
