f715c23c3746355720698ff200c52b649b8d3f1e
Nawras GEORGI <nawrasg@gmail.com>
2015-12-15 22:01:32 +0100

    Add daydream

---
+++ Android/src/fr/nawrasg/atlantis/other/BasicDreamService.java
@@ -0,0 +1,19 @@
+package fr.nawrasg.atlantis.other;
+
+import android.service.dreams.DreamService;
+
+import fr.nawrasg.atlantis.R;
+
+/**
+ * Created by Nawras GEORGI on 15/12/2015.
+ */
+public class BasicDreamService extends DreamService {
+
+	@Override
+	public void onDreamingStarted() {
+		super.onDreamingStarted();
+		setFullscreen(true);
+		setScreenBright(false);
+		setContentView(R.layout.dream_basic);
+	}
+}
