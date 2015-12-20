9c73f9e7203751f7899ddd0c06a17d78b43e4f6e
Nawras GEORGI <nawrasg@gmail.com>
2015-12-20 19:57:02 +0100

    Add const

--- Server/backend/classes/Player.php
+++ Server/backend/classes/Player.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/Settings.php';
 class Player {
+	const INCOMING_CALL = "incoming_call.mp3";
+	const INCOMING_MESSAGE = "incoming_message.mp3";
+	const NOTIFICATION = "notification.mp3";
 	function stop() {
 		exec ( 'sudo pkill mpg321' );
 	}
@@ -10,11 +13,10 @@ class Player {
 	}
 	function speak($string) {
 		$string = urlencode ( $string );
-		exec ( "curl 'http://translate.google.com/translate_tts?ie=UTF-8&q=$string&tl=fr&client=t' -H 'Referer: http://translate.google.com/' -H 'User-Agent: stagefright/1.2 (Linux;Android 5.0)' > msg.mp3" );
-		exec ( 'mpg321 msg.mp3' );
+		//exec ( "curl 'http://translate.google.com/translate_tts?ie=UTF-8&q=$string&tl=fr&client=t' -H 'Referer: http://translate.google.com/' -H 'User-Agent: stagefright/1.2 (Linux;Android 5.0)' > msg.mp3" );
+		//exec ( 'mpg321 msg.mp3' );
 	}
-	public function beep() {
-		$path = __DIR__ . "/../home/sound/notification.mp3";
-		exec ( "mpg321 $path" );
+	function sound($sound) {
+		$this->play ( __DIR__ . "/../home/sound/$sound" );
 	}
 }
