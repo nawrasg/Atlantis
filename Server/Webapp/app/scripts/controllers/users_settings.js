8508a2378e79f2879d02a6f38dfd26c7f44796a0
Nawras GEORGI <nawrasg@gmail.com>
2015-12-21 21:58:37 +0100

    Remove comments

--- Server/Webapp/app/scripts/controllers/users_settings.js
+++ Server/Webapp/app/scripts/controllers/users_settings.js
@@ -7,7 +7,7 @@
  */
 
 nApp.controller('UsersSettingsCtrl', function($scope, $http, $sessionStorage, $filter, $mdDialog, AtlantisUri) {
-//	var nUserAddDialog = '<md-dialog style=padding:20px><md-content><md-input-container><label>Nom</label><input ng-model=user.name class=dialog-close></md-input-container><md-input-container><label>Mot de passe</label><input ng-model=user.pwd class=dialog-close type=password></md-input-container><md-input-container><md-select ng-model=user.type.id placeholder=Type><md-option ng-value=type.id ng-repeat="type in types">{{type.label}}</md-option></md-select></md-input-container><md-input-container><label>Adresse mail</label><input ng-model=user.mail class=dialog-close></md-input-container><md-input-container><label>Téléphone</label><input ng-model=user.phone class=dialog-close></md-input-container></md-content><div class=md-actions><md-button ng-click=cancel()>Annuler</md-button><md-button class=md-primary ng-click=submit()>Ajouter</md-button></div></md-dialog>';
+	
 	$scope.addUser = function(e){
 		$mdDialog.show({
 			templateUrl: 'views/user_add.html',
