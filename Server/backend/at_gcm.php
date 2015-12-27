3aa42bed26246d27d401ffc2c240ac1d59ff0ac9
Nawras GEORGI <nawrasg@gmail.com>
2015-12-27 12:24:34 +0100

    " -> '

--- Server/backend/at_gcm.php
+++ Server/backend/at_gcm.php
@@ -1,5 +1,5 @@
 <?php
-header ( "Access-Control-Allow-Origin: *" );
+header ( 'Access-Control-Allow-Origin: *' );
 header ( 'Access-Control-Allow-Headers: origin, x-requested-with, content-type, accept' );
 header ( 'Access-Control-Allow-Methods: GET, POST, DELETE' );
 
