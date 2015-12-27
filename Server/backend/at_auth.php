0bb2d9df6e4f43f767340fa4ca2cc1192c838738
Nawras GEORGI <nawrasg@gmail.com>
2015-12-27 11:01:22 +0100

    Bug fix

--- Server/backend/at_auth.php
+++ Server/backend/at_auth.php
@@ -1,6 +1,6 @@
 <?php
-header ( "Access-Control-Allow-Origin: *" );
-header ( "Access-Control-Allow-Headers: Authorization" );
+header ( 'Access-Control-Allow-Origin: *' );
+header ( 'Access-Control-Allow-Headers: Authorization' );
 
 require_once __DIR__ . '/classes/connexion.php';
 
@@ -17,7 +17,7 @@ $bdd = getBDD ();
 $request = $bdd->query ( "SELECT * FROM at_users WHERE nom = '$username' AND password = '$pwd'" );
 $result = $request->fetch ();
 
-f (!$result) {
+if (!$result) {
 	http_response_code(403);
 }else{
 	http_response_code ( 202 );
