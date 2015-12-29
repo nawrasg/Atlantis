ce32168447d83e9c9f6e282bb9818f02ba3b0ae7
Nawras GEORGI <nawrasg@gmail.com>
2015-12-29 19:56:51 +0100

    Deprecated bug fix

--- Android/src/fr/nawrasg/atlantis/MainFragmentActivity.java
+++ Android/src/fr/nawrasg/atlantis/MainFragmentActivity.java
@@ -6,8 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -43,7 +43,7 @@ import fr.nawrasg.atlantis.preferences.MainPreferenceFragment;
 import fr.nawrasg.atlantis.type.DrawerItem;
 import fr.nawrasg.atlantis.type.DrawerSection;
 
-@SuppressWarnings("deprecation")
+
 public class MainFragmentActivity extends Activity implements OnItemClickListener {
 	private DrawerLayout nDrawerLayout;
 	private ListView nDrawerList;
@@ -63,12 +63,7 @@ public class MainFragmentActivity extends Activity implements OnItemClickListene
 		nDrawerList.setOnItemClickListener(this);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
-		nDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
-		nDrawerLayout, /* DrawerLayout object */
-		R.drawable.drawer, /* nav drawer image to replace 'Up' caret */
-		R.string.drawer_open, /* "open drawer" description for accessibility */
-		R.string.drawer_close /* "close drawer" description for accessibility */
-		) {
+		nDrawerToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
 			public void onDrawerClosed(View view) {
 				super.onDrawerClosed(view);
 				getActionBar().setTitle("Atlantis");
