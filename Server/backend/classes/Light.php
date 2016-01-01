06be35a990fa73d15490dc50e4a27fd13aa53da9
Nawras GEORGI <nawrasg@gmail.com>
2016-01-01 23:10:20 +0100

    Add allLights()/status() + isOn() -> multiple lights

--- Server/backend/classes/Light.php
+++ Server/backend/classes/Light.php
@@ -37,6 +37,23 @@ class Light {
 		}
 		return $light;
 	}
+	public static function allLights() {
+		$light = new Light ();
+		$light->lights = array ();
+		$bdd = getBDD ();
+		$req = $bdd->query ( "SELECT * FROM at_lights" );
+		while ( $data = $req->fetch () ) {
+			$light->lights [] = array (
+					'id' => $data ['id'],
+					'name' => $data ['name'],
+					'protocol' => $data ['protocol'],
+					'ip' => $data ['ip'],
+					'room' => $data ['room'],
+					'uid' => $data ['uid'] 
+			);
+		}
+		return $light;
+	}
 	public function color($color) {
 		$hue = new Hue ();
 		foreach ( $this->lights as $light ) {
@@ -74,16 +91,46 @@ class Light {
 		}
 	}
 	public function isOn() {
-		if (count ( $this->lights ) != 1) {
-			return false;
+		$size = count ( $this->lights );
+		$hue = new Hue ();
+		if ($size != 1) {
+			$status = array ();
+			for($i = 0; $i < $size; $i ++) {
+				$light = $this->lights [0];
+				$protocol = $light ['protocol'];
+				switch ($protocol) {
+					case 'hue' :
+						$uid = $light ['uid'];
+						array_push ( $status, $hue->isOn ( $uid ) );
+						break;
+				}
+			}
+			return $status;
 		}
 		$light = $this->lights [0];
 		$protocol = $light ['protocol'];
 		switch ($protocol) {
 			case 'hue' :
-				$hue = new Hue ();
 				$uid = $light ['uid'];
 				return $hue->isOn ( $uid );
+		}
+	}
+	public function status($pattern){
+		$size_lights = count($this->lights);
+		$size_pattern = count($pattern);
+		$hue = new Hue();
+		if($size_lights != $size_pattern){
+			return false;
+		}
+		for($i = 0; $i < $size_lights; $i++){
+			$light = $this->lights[$i]; 
+			$protocol = $light['protocol'];
+			switch($protocol){
+				case 'hue':
+					$uid = $light['uid'];
+					$hue->on($uid, $status[$i]);
+					break;
+			}
 		}
 	}
 }
