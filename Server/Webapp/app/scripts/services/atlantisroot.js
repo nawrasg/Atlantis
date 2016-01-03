5feb99cd3ef2358fefa9f9d4d287048f92c7278b
Nawras GEORGI <nawrasg@gmail.com>
2016-01-03 21:57:39 +0100

    First commit

---
+++ Server/Webapp/app/scripts/services/atlantisroot.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name atlantisWebAppApp.AtlantisRoot
+ * @description # AtlantisRoot Service in the atlantisWebAppApp.
+ */
+
+nApp.service('AtlantisRoot', function() {
+	return {};
+});
