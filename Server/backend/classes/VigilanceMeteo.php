558335d71a69d52f53e8bd52ccc1431535363dae
Nawras GEORGI <nawrasg@gmail.com>
2016-01-03 14:59:16 +0100

    Implement VigilanceMeteo

--- Server/backend/classes/VigilanceMeteo.php
+++ Server/backend/classes/VigilanceMeteo.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/Settings.php';
 
 /**
  * Based on: http://vigilance.meteofrance.com/data/NXFR34_LFPW_.xml
@@ -9,37 +10,78 @@ class VigilanceMeteo {
 
     var $url = "http://vigilance.meteofrance.com/data/NXFR34_LFPW_.xml";
     var $data;
+    var $dep;
+    var $meteo;
 
     public function __construct() {
-        $file = file_get_contents($this->url);
-        $xml = simplexml_load_string($file);
-        $json = json_encode($xml);
-        $this->data = json_decode($json);
-    }
-
-    public function getDate() {
-        return $this->data->entetevigilance->{'@attributes'}->dateinsert;
-    }
-
-    public function getColor($departement) {
-        $data = $this->data->datavigilance;
-        for ($i = 0; $i < count($data); $i++) {
-            $dep = $data[$i]->{'@attributes'}->dep;
-            if($dep == $departement){
-                $color = $data[$i]->{'@attributes'}->couleur;
-                switch($color){
-                    case '1':
-                        return 'vert';
-                    case '2':
-                        return 'jaune';
-                    case '3':
-                        return 'orange';
-                    case '4':
-                        return 'rouge';
-                }
-            }
+        $xml = simplexml_load_file($this->url);
+        $this->data = $xml;
+        $this->dep = (new Settings())->getSettings('Atlantis', 'dep');
+        $size = count($xml);
+        foreach($xml as $dep){
+        	if($dep['dep'] == $this->dep){
+				$this->meteo = $dep;
+        	}
         }
-        return FALSE;
+    }
+
+    function getVigilance(){
+    	$value = $this->meteo['couleur'];
+    	return $this->getColor($value);
+    }
+    
+    function getDepartement(){
+    	return $this->meteo['dep'];
+    }
+    
+    function getRisk(){
+    	if(isset($this->meteo->crue)){
+    		$crue = $this->getColor($this->meteo->crue['valeur']);
+    	}else{
+    		$crue = 'na';
+    	}
+    	if(isset($this->meteo->risque)){
+    		$risque = $this->getRisque($this->meteo->risque['valeur']);
+    	}else{
+    		$risque = 'na';
+    	}
+    	return array (
+    			'vigilance' => $this->getVigilance(),
+				'crue' => $crue,
+				'risque' => $risque 
+		);
+    }
+    
+    private function getColor($value){
+    	switch($value){
+    		case '1':
+    			return 'vert';
+    		case '2':
+    			return 'jaune';
+    		case '3':
+    			return 'orange';
+    		case '4':
+    			return 'rouge';
+    	}
+    }
+    
+    private function getRisque($value){
+    	switch($value){
+    		case '1':
+    			return 'vent';
+    		case '2':
+    			return 'pluie-inondation';
+    		case '3':
+    			return 'orages';
+    		case '4':
+    			return 'inondations';
+    		case '5':
+    			return 'neige-verglas';
+    		case '6':
+    			return 'canicule';
+    		case '7':
+    			return 'grand froid';
+    	}
     }
 
 }
