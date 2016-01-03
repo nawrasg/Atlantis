58bfd47ebdb45b60ecff69335f6a9b6d5f6941bd
Nawras GEORGI <nawrasg@gmail.com>
2016-01-03 21:57:57 +0100

    Add AtlantisRoot

--- Server/Webapp/app/scripts/controllers/mode.js
+++ Server/Webapp/app/scripts/controllers/mode.js
@@ -6,11 +6,17 @@
  * @description # ModeCtrl Controller of the atlantisWebAppApp
  */
 
-nApp.controller('ModeCtrl', function($scope, $http, $sessionStorage, AtlantisUri) {
+nApp.controller('ModeCtrl', function($scope, $http, $sessionStorage, $mdToast, AtlantisUri, AtlantisRoot) {
+	$scope.atlantis = AtlantisRoot;
 	$scope.setMode = function(mode){
 		var nURL = AtlantisUri.Home() + '?api=' + $sessionStorage.api + '&mode=' + mode;
 		$http.put(nURL).success(function(data, status){
-			//TODO
+			if(status == 202){
+				$scope.atlantis.mode = mode;
+			}else{
+				showToast($mdToast, 'Impossible de changer de mode (erreur ' + status + ') !')
+			}
+			
 		});
 	}
 });
