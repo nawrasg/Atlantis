dbc6a2dc20c38a7368e8eb44bce37abb61698011
Nawras GEORGI <nawrasg@gmail.com>
2016-01-04 22:19:51 +0100

    Add night mode hours

--- Server/Webapp/app/scripts/controllers/status_settings.js
+++ Server/Webapp/app/scripts/controllers/status_settings.js
@@ -30,6 +30,20 @@ nApp.controller('StatusSettingsCtrl', function($scope, $http, $sessionStorage, $
 			}
 		})
 	};
+	$scope.saveNightModeTime = function(){
+		var from = $scope.night.from;
+		var to = $scope.night.to;
+		var fromDate = new Date(from);
+		var toDate = new Date(to);
+		var nURL = AtlantisUri.System() + '?api=' + $sessionStorage.api;
+		nURL += '&nightFrom=' + fromDate.getHours() + ':' + fromDate.getMinutes() + '&nightTo=' + toDate.getHours() + ':' + toDate.getMinutes();
+		$http.put(nURL).success(function(data, status){
+			console.log(status, data);
+			if(status == 202){
+				showToast($mdToast, 'Horaires modifiés avec succès !');
+			}
+		});
+	};
 	function get(){
 		var nURL = AtlantisUri.System() + '?api=' + $sessionStorage.api;
 		$http.get(nURL).success(function(data, status){
@@ -40,6 +54,9 @@ nApp.controller('StatusSettingsCtrl', function($scope, $http, $sessionStorage, $
 				}else{
 					$scope.daemon = 'A l\'arrêt.';
 				}
+				var from = data.nightFrom.split(':');
+				var to = data.nightTo.split(':');
+				$scope.night = {from:new Date(1970, 0, 1, from[0], from[1]), to:new Date(1970, 0, 1, to[0], to[1])};
 			}
 		});
 	}
