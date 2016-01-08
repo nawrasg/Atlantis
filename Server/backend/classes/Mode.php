d71cd2a5ce98d8d3c12654db852d84ffadc8b304
Nawras GEORGI <nawrasg@gmail.com>
2016-01-08 22:05:57 +0100

    Add scenarios for mode

--- Server/backend/classes/Mode.php
+++ Server/backend/classes/Mode.php
@@ -8,9 +8,28 @@ class Mode{
 	
 	function setMode($mode){
 		(new Settings())->setSettings('Mode', 'mode', $mode);
+		$this->exec_scenario($mode);
 	}
 	
 	function getMode(){
 		return ((new Settings())->getSettings('Mode', 'mode'));
+	}
+	
+	private function exec_scenario($mode) {
+		switch($mode){
+			case Mode::DAY:
+				$scenario = __DIR__ . '/../scenarios/mode_day.php';
+				break;
+			case Mode::NIGHT:
+				$scenario = __DIR__ . '/../scenarios/mode_night.php';
+				break;
+			case Mode::AWAY:
+				$scenario = __DIR__ . '/../scenarios/mode_away.php';
+				break;
+				
+		}
+		if (file_exists ( $scenario )) {
+			exec ( "nohup php $scenario >/dev/null 2>&1 &" );
+		}
 	}
 }
