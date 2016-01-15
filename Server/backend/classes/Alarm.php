07311ef7a4b960cd5231977e993c611082511d27
Nawras GEORGI <nawrasg@gmail.com>
2016-01-15 22:59:35 +0100

    Add cameras photos option

--- Server/backend/classes/Alarm.php
+++ Server/backend/classes/Alarm.php
@@ -19,13 +19,18 @@ class Alarm {
 			$this->on ();
 		}
 	}
-	public function execAlarm(){
-		$this->exec_scenario();
+	public function execAlarm() {
+		$this->exec_scenario ();
 	}
 	private function exec_scenario() {
 		$scenario = __DIR__ . '/../scenarios/alarm.php';
 		if (file_exists ( $scenario )) {
 			exec ( "nohup php $scenario >/dev/null 2>&1 &" );
+		}
+		$cameras_photos = (new Settings ())->getSettings ( 'Security', 'photos' );
+		if ($cameras_photos) {
+			$path = __DIR__ . '/../script/cameras.php';
+			exec ( "nohup php $path >/dev/null 2>&1 &" );
 		}
 	}
 }
