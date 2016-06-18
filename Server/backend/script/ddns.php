7b158c7c930d73db1805df3991ef5bf2d032c59c
Nawras GEORGI <support@nawrasg.fr>
2016-06-18 11:45:18 +0200

    Update on IP change only

--- Server/backend/script/ddns.php
+++ Server/backend/script/ddns.php
@@ -10,14 +10,20 @@ if($settings->getSettings('DDNS', 'on')){
 	$username = $settings->getSettings('DDNS', 'username');
 	$password = $settings->getSettings('DDNS', 'password');
 	$ip = urlencode(file_get_contents('http://nawrasg.fr/ip.php'));
-	$url = "http://www.ovh.com/nic/update?system=dyndns&hostname=$host&myip=$ip";
+	$last_ip = $settings->getSettings('DDNS', 'last_ip');
 	
-	$ch = curl_init ( $url );
-	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-	$response = curl_exec ( $ch );
-	curl_close($ch);
-	(new Log())->log(Log::INFO, __FILE__, $response);
+	if($ip == $last_ip){
+		(new Log())->log(Log::INFO, __FILE__, "No IP Change $ip");
+	}else{
+		$url = "http://www.ovh.com/nic/update?system=dyndns&hostname=$host&myip=$ip";
+		$ch = curl_init ( $url );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		$response = curl_exec ( $ch );
+		curl_close($ch);
+		(new Log())->log(Log::INFO, __FILE__, $response);
+		$settings->setSettings('DDNS', 'last_ip', $ip);
+	}
 }
