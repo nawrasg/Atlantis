1a8162118021d46d32d74d8f675744f68e687850
Nawras GEORGI <support@nawrasg.fr>
2016-06-18 22:52:30 +0200

    Add owncloud option

--- Server/Webapp/app/scripts/controllers/user_add.js
+++ Server/Webapp/app/scripts/controllers/user_add.js
@@ -7,16 +7,9 @@
  */
 
 nApp.controller('UserAddCtrl', function($scope, $http, $sessionStorage, $mdDialog, $mdToast, AtlantisUri, user) {
-	$scope.types = [{
-		id:0,
-		label:'Administrateur'
-	},{
-		id:1,
-		label: 'Utilisateur'
-	},{
-		id:2,
-		label: 'Visiteur'
-	}];
+	$scope.types = [{id:0, label:'Administrateur'},
+	                {id:1, label: 'Utilisateur'},
+	                {id:2, label: 'Visiteur'}];
 	if(user != null){
 		$scope.user = {name: user.nom, phone: user.phone, mail: user.mail, type: {id:user.type}};
 		$scope.btnSubmit = "Modifier";
@@ -34,7 +27,8 @@ nApp.controller('UserAddCtrl', function($scope, $http, $sessionStorage, $mdDialo
 	function add(){
 		var nURL = AtlantisUri.Users() + '?api=' + $sessionStorage.api;
 		if(user == null){
-			nURL += '&name=' + $scope.user.name + '&type=' + $scope.user.type.id + '&pwd=' + $scope.user.pwd;
+			nURL += '&name=' + $scope.user.name + '&type=' + $scope.user.type.id + '&pwd=' + $scope.user.pwd; 
+			nURL += '&mail=' + $scope.user.mail + '&phone=' + $scope.user.phone + '&owncloud=' + $scope.user.owncloud;
 			$http.post(nURL).success(function(data, status){
 				if(status == 202){
 					$mdDialog.hide(data);
