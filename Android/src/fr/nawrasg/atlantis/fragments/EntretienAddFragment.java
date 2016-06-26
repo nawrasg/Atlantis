f61e2f7e6cd25180d558207d21ea46e8a95168c8
Nawras GEORGI <support@nawrasg.fr>
2016-06-26 23:15:36 +0200

    Switch to okhttp

--- Android/src/fr/nawrasg/atlantis/fragments/EntretienAddFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/EntretienAddFragment.java
@@ -15,7 +15,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.DateFormat;
@@ -30,7 +37,6 @@ import butterknife.OnClick;
 import fr.nawrasg.atlantis.App;
 import fr.nawrasg.atlantis.MainFragmentActivity;
 import fr.nawrasg.atlantis.R;
-import fr.nawrasg.atlantis.async.DataPOST;
 
 public class EntretienAddFragment extends Fragment {
 	@Bind(R.id.txtEntretienNom)
@@ -43,12 +49,14 @@ public class EntretienAddFragment extends Fragment {
 	private DateFormat fmtDate = DateFormat.getDateInstance();
 	private SimpleDateFormat nDate = new SimpleDateFormat("yyyy-MM-dd", Locale.FRANCE);
 	private Context mContext;
+	private OkHttpClient mClient;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View nView = inflater.inflate(R.layout.fragment_entretien_add, container, false);
 		ButterKnife.bind(this, nView);
 		mContext = getActivity();
+		mClient = new OkHttpClient();
 		setHasOptionsMenu(true);
 		return nView;
 	}
@@ -94,11 +102,31 @@ public class EntretienAddFragment extends Fragment {
 			String nom = URLEncoder.encode(txtNom.getText().toString(), "UTF-8");
 			String qte = txtQte.getText().toString();
 			String date = nDate.format(nCalendar.getTime());
-			new DataPOST(mContext).execute(App.ENTRETIEN, "nom=" + nom + "&peremption=" + date + "&qte=" + qte);
+			String nData = "nom=" + nom + "&peremption=" + date + "&qte=" + qte;
+			postItem(nData);
 			setNew();
 		} catch (UnsupportedEncodingException e) {
 			Toast.makeText(mContext, "Erreur : " + e.getMessage(), Toast.LENGTH_LONG).show();
 		}
+	}
+
+	private void postItem(String data){
+		String nURL = App.getFullUrl(mContext) + App.ENTRETIEN + "?api=" + App.getAPI(mContext) + "&" + data;
+		Request nRequest = new Request.Builder()
+				.url(nURL)
+				.post(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), ""))
+				.build();
+		mClient.newCall(nRequest).enqueue(new Callback() {
+			@Override
+			public void onFailure(Request request, IOException e) {
+
+			}
+
+			@Override
+			public void onResponse(Response response) throws IOException {
+
+			}
+		});
 	}
 
 	@OnClick(R.id.btnEntretienAddScan)
