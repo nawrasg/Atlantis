109bc2ba56c646f7fe0682fc5414b683909b3771
Nawras GEORGI <support@nawrasg.fr>
2016-06-26 22:47:28 +0200

    Add ean

---
+++ Server/backend/at_sync.php
@@ -0,0 +1,33 @@
+<?php
+header ( "Access-Control-Allow-Origin: *" );
+
+require_once __DIR__ . '/classes/connexion.php';
+require_once __DIR__ . '/classes/checkAPI.php';
+
+$page_level = 1;
+
+
+if (isset ( $_REQUEST ['api'] ) && checkAPI ( $_REQUEST ['api'], $page_level )) {
+	switch ($_SERVER ['REQUEST_METHOD']) {
+		case 'GET' :
+			http_response_code(202);
+			echo json_encode(get());			
+			break;
+	}
+} else {
+	http_response_code ( 403 );
+}
+
+function get() {
+	return array (
+			'ean' => getEan () 
+	);
+}
+
+function getEan() {
+	$bdd = getBDD ();
+	$req = $bdd->query ( 'SELECT * FROM at_ean ORDER BY nom' );
+	$result = $req->fetchAll(PDO::FETCH_ASSOC);
+	$req->closeCursor ();
+	return $result;
+}
