b954ae3848e05f0f10a5b75cebba66f23a82b672
Nawras GEORGI <support@nawrasg.fr>
2016-06-26 23:54:46 +0200

    Switch to OkHttp

--- Android/src/fr/nawrasg/atlantis/fragments/LightFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/LightFragment.java
@@ -1,14 +1,10 @@
 package fr.nawrasg.atlantis.fragments;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,12 +14,23 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import fr.nawrasg.atlantis.App;
 import fr.nawrasg.atlantis.R;
 import fr.nawrasg.atlantis.adapters.LightAdapter;
-import fr.nawrasg.atlantis.async.DataGET;
-import fr.nawrasg.atlantis.async.DataPUT;
 import fr.nawrasg.atlantis.fragments.dialogs.LightDialogFragment;
 import fr.nawrasg.atlantis.type.Hue;
 import fr.nawrasg.atlantis.type.Light;
@@ -33,11 +40,13 @@ public class LightFragment extends ListFragment{
 	private Context mContext;
 	private ArrayList<Light> mList;
 	private ArrayList<Room> mRoomList;
+	private Handler mHandler;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View nView = inflater.inflate(R.layout.fragment_lights, container, false);
 		mContext = getActivity();
+		mHandler = new Handler();
 		getItems();
 		setHasOptionsMenu(true);
 		return nView;
@@ -66,11 +75,65 @@ public class LightFragment extends ListFragment{
 	}
 
 	private void getItems() {
-		new LightsGET(mContext).execute(App.LIGHTS);
+		String nURL = App.getFullUrl(mContext) + App.LIGHTS + "?api=" + App.getAPI(mContext);
+		Request nRequest = new Request.Builder()
+				.url(nURL)
+				.build();
+		App.httpClient.newCall(nRequest).enqueue(new Callback() {
+			@Override
+			public void onFailure(Request request, IOException e) {
+
+			}
+
+			@Override
+			public void onResponse(Response response) throws IOException {
+				mList = new ArrayList<>();
+				mRoomList = new ArrayList<>();
+				try {
+					JSONObject nJson = new JSONObject(response.body().string());
+					JSONArray arr = nJson.getJSONArray("lights");
+					for (int i = 0; i < arr.length(); i++) {
+						JSONObject json = arr.getJSONObject(i);
+						Light nLight = new Hue(json);
+						mList.add(nLight);
+
+					}
+					JSONArray nArr = nJson.getJSONArray("rooms");
+					for (int i = 0; i < nArr.length(); i++) {
+						Room nRoom = new Room(nArr.getJSONObject(i));
+						mRoomList.add(nRoom);
+					}
+				} catch (JSONException e) {
+					Log.e("Atlantis", e.toString());
+				}
+				mHandler.post(new Runnable() {
+					@Override
+					public void run() {
+						setListAdapter(new LightAdapter(mContext, mList, mRoomList));
+						setItemListener();
+					}
+				});
+			}
+		});
 	}
 
 	private void toggleLight(Light light) {
-		new DataPUT(mContext).execute(App.LIGHTS, "toggle=" + ((Hue)light).getUID());
+		String nURL = App.getFullUrl(mContext) + App.LIGHTS + "?api=" + App.getAPI(mContext) + "&toggle=" + ((Hue)light).getUID();
+		Request nRequest = new Request.Builder()
+				.url(nURL)
+				.put(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), ""))
+				.build();
+		App.httpClient.newCall(nRequest).enqueue(new Callback() {
+			@Override
+			public void onFailure(Request request, IOException e) {
+
+			}
+
+			@Override
+			public void onResponse(Response response) throws IOException {
+
+			}
+		});
 	}
 
 	private void setItemListener() {
@@ -103,39 +166,4 @@ public class LightFragment extends ListFragment{
 		nBundle.putParcelable("light", light);
 		return nBundle;
 	}
-
-	private class LightsGET extends DataGET {
-
-		public LightsGET(Context context) {
-			super(context);
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			mList = new ArrayList<>();
-			mRoomList = new ArrayList<>();
-			try {
-				JSONObject nJson = new JSONObject(result);
-				JSONArray arr = nJson.getJSONArray("lights");
-				for (int i = 0; i < arr.length(); i++) {
-					JSONObject json = arr.getJSONObject(i);
-					Light nLight = new Hue(json);
-					mList.add(nLight);
-					
-				}
-				JSONArray nArr = nJson.getJSONArray("rooms");
-				for (int i = 0; i < nArr.length(); i++) {
-					Room nRoom = new Room(nArr.getJSONObject(i));
-					mRoomList.add(nRoom);
-				}
-			} catch (JSONException e) {
-				Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
-			}
-			setListAdapter(new LightAdapter(mContext, mList, mRoomList));
-			setItemListener();
-		}
-
-	}
-
 }
