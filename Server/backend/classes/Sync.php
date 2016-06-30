2e8c19f1b819b1d686e1b48c7873b58733e0adc3
Nawras GEORGI <support@nawrasg.fr>
2016-06-30 12:34:13 +0200

    First commit

---
+++ Server/backend/classes/Sync.php
@@ -0,0 +1,25 @@
+<?php
+class Sync{
+	const SCENARIOS = 'scenarios';
+	
+	private $data;
+	private $file = 'sync.json';
+	
+	function __construct(){
+		$json = file_get_contents($this->file, TRUE);
+		$this->data = json_decode($json);
+	}
+	
+	private function save(){
+		file_put_contents($this->file, json_encode($this->data), FILE_USE_INCLUDE_PATH);
+	}
+	
+	function update($section){
+		$this->data->$section = time();
+		$this->saveSettings();
+	}
+	
+	function get($section){
+		return $this->data->$section;
+	}
+}
