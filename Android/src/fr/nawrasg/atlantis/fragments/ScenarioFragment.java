cf9d11b6a998f80ccb6c3b9b38a1f1dfde52821e
Nawras GEORGI <support@nawrasg.fr>
2016-06-30 16:54:27 +0200

    Remove live getItems()

--- Android/src/fr/nawrasg/atlantis/fragments/ScenarioFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/ScenarioFragment.java
@@ -5,24 +5,13 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
-import fr.nawrasg.atlantis.App;
 import fr.nawrasg.atlantis.R;
 import fr.nawrasg.atlantis.adapters.ScenarioAdapter;
 import fr.nawrasg.atlantis.other.AtlantisContract;
@@ -35,14 +24,12 @@ public class ScenarioFragment extends ListFragment {
 	private Context mContext;
 	private ArrayList<Scenario> mList;
 	private ScenarioAdapter mAdapter;
-	private Handler mHandler;
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View nView = inflater.inflate(R.layout.fragment_scenario, container, false);
 		mContext = getActivity();
-		mHandler = new Handler();
 		return nView;
 	}
 
@@ -64,37 +51,5 @@ public class ScenarioFragment extends ListFragment {
 		}
 		mAdapter = new ScenarioAdapter(mContext, mList);
 		setListAdapter(mAdapter);
-	}
-
-	private void getItemsLegacy(){
-		String nURL = App.getFullUrl(mContext) + App.SCENARIOS + "?api=" + App.getAPI(mContext);
-		Request nRequest = new Request.Builder().url(nURL).build();
-		App.httpClient.newCall(nRequest).enqueue(new Callback() {
-			@Override
-			public void onFailure(Request request, IOException e) {
-
-			}
-
-			@Override
-			public void onResponse(Response response) throws IOException {
-				mList = new ArrayList<>();
-				try {
-					JSONArray nArr = new JSONArray(response.body().string());
-					for(int i = 0; i < nArr.length(); i++){
-						Scenario nScenario = new Scenario(nArr.getJSONObject(i));
-						mList.add(nScenario);
-					}
-					mAdapter = new ScenarioAdapter(mContext, mList);
-					mHandler.post(new Runnable() {
-						@Override
-						public void run() {
-							setListAdapter(mAdapter);
-						}
-					});
-				} catch (JSONException e) {
-					Log.e("Atlantis", e.toString());
-				}
-			}
-		});
 	}
 }
