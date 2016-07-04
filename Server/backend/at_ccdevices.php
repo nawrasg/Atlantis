374440381e770fa4690dd6bc4932548f90b12161
Nawras GEORGI <support@nawrasg.fr>
2016-07-04 12:37:10 +0200

    Add sync class

--- Server/backend/at_ccdevices.php
+++ Server/backend/at_ccdevices.php
@@ -5,6 +5,7 @@ header ( 'Access-Control-Allow-Methods: GET, POST, PUT, DELETE' );
 
 require_once __DIR__ . '/classes/connexion.php';
 require_once __DIR__ . '/classes/checkAPI.php';
+require_once __DIR__ . '/classes/Sync.php';
 
 $page_level = 0;
 
