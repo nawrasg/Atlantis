97ead09038da5281032362d336f8a65b3961a292
Nawras GEORGI <support@nawrasg.fr>
2016-07-05 19:16:47 +0200

    Add user

--- Android/src/fr/nawrasg/atlantis/type/GPS.java
+++ Android/src/fr/nawrasg/atlantis/type/GPS.java
@@ -1,40 +1,42 @@
 package fr.nawrasg.atlantis.type;
 
+import android.util.Log;
+
 import org.json.JSONObject;
 
 public class GPS {
-	private String mUser, mMAC, mDate;
-	private double mLat, mLong;
-	
-	public GPS(JSONObject json){
-		try{
-			//mUser = json.getString("username");
-			mMAC = json.getString("mac");
-			mLat = json.getDouble("lat");
-			mLong = json.getDouble("long");
-			mDate = json.getString("timestamp");
-		}catch(Exception e){
-			
-		}
-	}
-	
-	/*public String getUser(){
-		return mUser;
-	}*/
-	
-	public String getMAC(){
-		return mMAC;
-	}
-	
-	public double getLatitude(){
-		return mLat;
-	}
-	
-	public double getLongitude(){
-		return mLong;
-	}
-	
-	public String getTimestamp(){
-		return mDate;
-	}
+    private String mUser, mMAC, mDate;
+    private double mLat, mLong;
+
+    public GPS(JSONObject json) {
+        try {
+            mUser = json.getString("nom");
+            mMAC = json.getString("mac");
+            mLat = json.getDouble("lat");
+            mLong = json.getDouble("long");
+            mDate = json.getString("timestamp");
+        } catch (Exception e) {
+            Log.e("Atlantis", e.toString());
+        }
+    }
+
+    public String getUser() {
+        return mUser;
+    }
+
+    public String getMAC() {
+        return mMAC;
+    }
+
+    public double getLatitude() {
+        return mLat;
+    }
+
+    public double getLongitude() {
+        return mLong;
+    }
+
+    public String getTimestamp() {
+        return mDate;
+    }
 }
