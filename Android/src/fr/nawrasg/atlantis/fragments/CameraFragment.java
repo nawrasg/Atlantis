5b78ff5f81bfa40379037571561a840373932095
Nawras GEORGI <support@nawrasg.fr>
2016-07-09 00:14:42 +0200

    Switch to App.getUri(...)

--- Android/src/fr/nawrasg/atlantis/fragments/CameraFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/CameraFragment.java
@@ -33,59 +33,59 @@ import fr.nawrasg.atlantis.type.Camera;
  * Created by Nawras GEORGI on 17/11/2015.
  */
 public class CameraFragment extends Fragment {
-	private Context mContext;
-	@Bind(R.id.rvCamera)
-	RecyclerView mRecyclerView;
-	private ArrayList<Camera> mList;
-	private Handler mHandler;
+    private Context mContext;
+    @Bind(R.id.rvCamera)
+    RecyclerView mRecyclerView;
+    private ArrayList<Camera> mList;
+    private Handler mHandler;
 
-	@Nullable
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View nView = inflater.inflate(R.layout.fragment_camera, container, false);
-		ButterKnife.bind(this, nView);
-		mContext = getActivity();
-		mHandler = new Handler();
-		return nView;
-	}
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View nView = inflater.inflate(R.layout.fragment_camera, container, false);
+        ButterKnife.bind(this, nView);
+        mContext = getActivity();
+        mHandler = new Handler();
+        return nView;
+    }
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-		getItems();
-	}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        getItems();
+    }
 
-	private void getItems(){
-		String nURL = App.getFullUrl(mContext) + App.CAMERAS + "?api=" + App.getAPI(mContext);
-		Request nRequest = new Request.Builder().url(nURL).build();
-		App.httpClient.newCall(nRequest).enqueue(new Callback() {
-			@Override
-			public void onFailure(Request request, IOException e) {
+    private void getItems() {
+        String nURL = App.getUri(mContext, App.CAMERAS);
+        Request nRequest = new Request.Builder().url(nURL).build();
+        App.httpClient.newCall(nRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
-			}
+            }
 
-			@Override
-			public void onResponse(Response response) throws IOException {
-				if(response.code() == 202){
-					mList = new ArrayList<Camera>();
-					try {
-						JSONArray nArr = new JSONArray(response.body().string());
-						for (int i = 0; i < nArr.length(); i++) {
-							Camera nCamera = new Camera(nArr.getJSONObject(i));
-							mList.add(nCamera);
-						}
-						mHandler.post(new Runnable() {
-							@Override
-							public void run() {
-								mRecyclerView.setAdapter(new CameraAdapter(mContext, mList));
-							}
-						});
-					} catch (JSONException e) {
-						Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
-					}
-				}
-			}
-		});
-	}
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (response.code() == 202) {
+                    mList = new ArrayList<Camera>();
+                    try {
+                        JSONArray nArr = new JSONArray(response.body().string());
+                        for (int i = 0; i < nArr.length(); i++) {
+                            Camera nCamera = new Camera(nArr.getJSONObject(i));
+                            mList.add(nCamera);
+                        }
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRecyclerView.setAdapter(new CameraAdapter(mContext, mList));
+                            }
+                        });
+                    } catch (JSONException e) {
+                        Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
 }
