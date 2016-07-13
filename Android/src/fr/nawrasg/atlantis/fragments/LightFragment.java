d4667e53299670ba2a97aecfb2830239baddf291
Nawras GEORGI <support@nawrasg.fr>
2016-07-13 23:17:13 +0200

    Remove refresh

--- Android/src/fr/nawrasg/atlantis/fragments/LightFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/LightFragment.java
@@ -8,9 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -52,24 +49,7 @@ public class LightFragment extends ListFragment{
 		mContext = getActivity();
 		mHandler = new Handler();
 		getItems();
-		setHasOptionsMenu(true);
 		return nView;
-	}
-
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		super.onCreateOptionsMenu(menu, inflater);
-		inflater.inflate(R.menu.fragment_light, menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.itemLightRefresh:
-				getItems();
-				return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	private void getItems(){
