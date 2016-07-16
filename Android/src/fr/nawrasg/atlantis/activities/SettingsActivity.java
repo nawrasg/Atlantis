2629cc59b0d696a33057e1674e629f284d4f5220
Nawras GEORGI <support@nawrasg.fr>
2016-07-17 00:19:06 +0200

    Add settings PIN

--- Android/src/fr/nawrasg/atlantis/activities/SettingsActivity.java
+++ Android/src/fr/nawrasg/atlantis/activities/SettingsActivity.java
@@ -1,16 +1,30 @@
 package fr.nawrasg.atlantis.activities;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URLEncoder;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import fr.nawrasg.atlantis.App;
 import fr.nawrasg.atlantis.R;
 import fr.nawrasg.atlantis.adapters.TabsAdapter;
 import fr.nawrasg.atlantis.preferences.ConnectionPreferenceFragment;
@@ -22,6 +36,7 @@ import fr.nawrasg.atlantis.preferences.SecurityPreferenceFragment;
  */
 
 public class SettingsActivity extends AppCompatActivity {
+    Context mContext;
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
     @Bind(R.id.tabsSettingsTabs)
@@ -33,19 +48,23 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mContext = this;
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setupViewPager();
         mTabs.setupWithViewPager(mViewPager);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener(){
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+        if (App.isPIN(mContext)) {
+            enterPIN();
+        }
     }
 
     private void setupViewPager() {
@@ -54,5 +73,42 @@ public class SettingsActivity extends AppCompatActivity {
         nAdapter.addTab(new SecurityPreferenceFragment(), getString(R.string.xml_pref_main_security_title));
         nAdapter.addTab(new DiversPreferenceFragment(), getString(R.string.xml_pref_main_divers_title));
         mViewPager.setAdapter(nAdapter);
+    }
+
+    public void enterPIN() {
+        View view = getLayoutInflater().inflate(R.layout.fragment_dialog_pin, null);
+        final EditText txtCode = (EditText) view.findViewById(R.id.txtPin);
+        final TextInputLayout txtCodeLayout = (TextInputLayout) view.findViewById(R.id.txtPinLayout);
+        
+        AlertDialog.Builder inputBoxBuilder = new AlertDialog.Builder(this);
+        inputBoxBuilder.setView(view);
+        inputBoxBuilder.setCancelable(false);
+        inputBoxBuilder.setPositiveButton(getResources().getString(R.string.fragment_settings_pin_login), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setNegativeButton(getResources().getString(R.string.fragment_settings_pin_cancel), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        final AlertDialog inputBox = inputBoxBuilder.create();
+        inputBox.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        inputBox.show();
+        inputBox.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtCode.getText().toString().equals(App.getString(mContext, "pinCode"))) {
+                    inputBox.dismiss();
+                } else {
+                    txtCode.setText("");
+                    txtCodeLayout.setError(getString(R.string.fragment_settings_pin_password_incorrect));
+                }
+            }
+        });
     }
 }
