3994a6e192bf236bca3494fab90d7ba9aa84f936
Nawras GEORGI <support@nawrasg.fr>
2016-07-16 10:44:42 +0200

    Remove old drawer items

--- Android/src/fr/nawrasg/atlantis/interfaces/DrawerItemInterface.java
+++
@@ -1,9 +0,0 @@
-package fr.nawrasg.atlantis.interfaces;
-
-public interface DrawerItemInterface {
-	int getId();
-    String getLabel();
-    int getType();
-    boolean isEnabled();
-    boolean updateActionBarTitle();
-}
