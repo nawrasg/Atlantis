5d237fb59f9fa04b4e43c5517802a09c3fd6b7e7
Nawras GEORGI <support@nawrasg.fr>
2016-07-17 17:30:19 +0200

    Add setNavigationHeader()

--- Android/src/fr/nawrasg/atlantis/activities/MainActivity.java
+++ Android/src/fr/nawrasg/atlantis/activities/MainActivity.java
@@ -12,8 +12,12 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -65,17 +69,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         createSyncAccount();
         mNavigation.setItemIconTintList(null);
         mNavigation.setNavigationItemSelectedListener(this);
+        setNavigationHeader();
         setSupportActionBar(mToolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_white_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         loadFragment(new HomeFragment(), true);
         loadFragment(new PlantFragment(), false);
         handleIntent(getIntent());
-        if(App.getURL(mContext).equals("")){
+        if (App.getURL(mContext).equals("")) {
             startActivity(new Intent(this, SettingsActivity.class));
         }
     }
 
+    private void setNavigationHeader() {
+        View nView = mNavigation.getRootView();
+        TextView txtUsername = (TextView) nView.findViewById(R.id.txtNavigationUsername);
+        txtUsername.setText(App.getString(this, "username"));
+        TextView txtUserType = (TextView) nView.findViewById(R.id.txtNavigationUserType);
+        switch (App.getInt(this, "usertype")) {
+            case 0:
+                txtUserType.setText(getString(R.string.user_account_type_admin));
+                break;
+            case 1:
+                txtUserType.setText(getString(R.string.user_account_type_user));
+                break;
+            case 2:
+                txtUserType.setText(getString(R.string.user_account_type_visitor));
+                break;
+            case -1:
+                txtUserType.setText(getString(R.string.user_account_type_blocked));
+                break;
+        }
+    }
 
     private void createSyncAccount() {
         Account nAccount = new Account("Atlantis", "fr.nawrasg.atlantis");
