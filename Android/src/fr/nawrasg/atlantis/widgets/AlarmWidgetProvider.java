3f07b1e2005dc9eb3d39d0110a4f6db268879d27
Nawras GEORGI <support@nawrasg.fr>
2016-07-17 23:02:44 +0200

    Add alarm widget

---
+++ Android/src/fr/nawrasg/atlantis/widgets/AlarmWidgetProvider.java
@@ -0,0 +1,17 @@
+package fr.nawrasg.atlantis.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Nawras on 17/07/2016.
+ */
+
+public class AlarmWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
