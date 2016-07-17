f43949488c9462bced609566c5919c7e15e37c3e
Nawras GEORGI <support@nawrasg.fr>
2016-07-17 23:01:45 +0200

    Bug fix

--- Android/src/fr/nawrasg/atlantis/fragments/dialogs/DeviceInfoDialogFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/dialogs/DeviceInfoDialogFragment.java
@@ -32,81 +32,82 @@ import fr.nawrasg.atlantis.type.Device;
 import fr.nawrasg.atlantis.type.User;
 
 public class DeviceInfoDialogFragment extends DialogFragment {
-	private Context mContext;
-	@Bind(R.id.txtDeviceDialogTitle)
-	EditText txtTitle;
-	@Bind(R.id.txtDeviceDialogIp)
-	EditText txtIP;
-	@Bind(R.id.spDeviceDialogUser)
-	Spinner spUser;
-	private Device mDevice;
-	private ArrayList<User> mUserList;
+    private Context mContext;
+    @Bind(R.id.txtDeviceDialogTitle)
+    EditText txtTitle;
+    @Bind(R.id.txtDeviceDialogIp)
+    EditText txtIP;
+    @Bind(R.id.spDeviceDialogUser)
+    Spinner spUser;
+    private Device mDevice;
+    private ArrayList<User> mUserList;
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		mContext = getActivity();
-		LayoutInflater inflater = getActivity().getLayoutInflater();
-		View nView = inflater.inflate(R.layout.fragment_dialog_device_info, null);
-		ButterKnife.bind(this, nView);
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setPositiveButton(mContext.getString(R.string.fragment_dialog_button_save), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				try {
-					String nDeviceTitle = URLEncoder.encode(txtTitle.getText().toString(), "UTF-8");
-					String nDeviceIp = URLEncoder.encode(txtIP.getText().toString(), "UTF-8");
-					String nDeviceMac = URLEncoder.encode(mDevice.getMac(), "UTF-8");
-					int nPosition = spUser.getSelectedItemPosition();
-					String nDeviceOwner = URLEncoder.encode(mUserList.get(nPosition).getName(), "UTF-8");
-					String nURL = "title=" + nDeviceTitle + "&ip=" + nDeviceIp + "&mac=" + nDeviceMac + "&user=" + nDeviceOwner;
-					String nURLf = App.getUri(mContext, App.DEVICES) + "&" + nURL;
-					Request nRequest = new Request.Builder()
-							.url(nURLf)
-							.put(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), ""))
-							.build();
-					App.httpClient.newCall(nRequest).enqueue(new Callback() {
-						@Override
-						public void onFailure(Request request, IOException e) {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mContext = getActivity();
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View nView = inflater.inflate(R.layout.fragment_dialog_device_info, null);
+        ButterKnife.bind(this, nView);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setPositiveButton(mContext.getString(R.string.fragment_dialog_button_save), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                try {
+                    String nDeviceTitle = URLEncoder.encode(txtTitle.getText().toString(), "UTF-8");
+                    String nDeviceIp = URLEncoder.encode(txtIP.getText().toString(), "UTF-8");
+                    String nDeviceMac = URLEncoder.encode(mDevice.getMac(), "UTF-8");
+                    int nPosition = spUser.getSelectedItemPosition();
+                    String nDeviceOwner = URLEncoder.encode(mUserList.get(nPosition).getName(), "UTF-8");
+                    String nURL = "title=" + nDeviceTitle + "&ip=" + nDeviceIp + "&mac=" + nDeviceMac + "&user=" + nDeviceOwner;
+                    String nURLf = App.getUri(mContext, App.DEVICES) + "&" + nURL;
+                    Request nRequest = new Request.Builder()
+                            .url(nURLf)
+                            .put(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), ""))
+                            .build();
+                    App.httpClient.newCall(nRequest).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
 
-						}
+                        }
 
-						@Override
-						public void onResponse(Response response) throws IOException {
-							dismiss();
-						}
-					});
-				} catch (UnsupportedEncodingException e) {
-					Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
-				}
-				dismiss();
-			}
-		}).setNegativeButton(mContext.getString(R.string.fragment_dialog_button_cancel), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				dismiss();
-			}
-		}).setView(nView);
-		onViewCreated(nView, savedInstanceState);
-		return builder.create();
-	}
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+                            dismiss();
+                        }
+                    });
+                } catch (UnsupportedEncodingException e) {
+                    Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
+                }
+                dismiss();
+            }
+        }).setNegativeButton(mContext.getString(R.string.fragment_dialog_button_cancel), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dismiss();
+            }
+        }).setView(nView);
+        onViewCreated(nView, savedInstanceState);
+        return builder.create();
+    }
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		getSettings();
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getSettings();
+    }
 
-	}
-
-	private void getSettings() {
-		Bundle nBundle = getArguments();
-		mDevice = nBundle.getParcelable("device");
-		mUserList = nBundle.getParcelableArrayList("users");
-		txtTitle.setText(mDevice.getNom());
-		txtIP.setText(mDevice.getIP());
-		spUser.setAdapter(new UserAdapter(mContext, mUserList));
-		for (int i = 0; i < mUserList.size(); i++) {
-			if (mUserList.get(i).getName().equals(mDevice.getOwner())) {
-				spUser.setSelection(i);
-				break;
-			}
-		}
-	}
+    private void getSettings() {
+        Bundle nBundle = getArguments();
+        mDevice = nBundle.getParcelable("device");
+        mUserList = nBundle.getParcelableArrayList("users");
+        txtTitle.setText(mDevice.getNom());
+        txtIP.setText(mDevice.getIP());
+        if (mUserList != null) {
+            spUser.setAdapter(new UserAdapter(mContext, mUserList));
+            for (int i = 0; i < mUserList.size(); i++) {
+                if (mUserList.get(i).getID() == mDevice.getUser()) {
+                    spUser.setSelection(i);
+                    break;
+                }
+            }
+        }
+    }
 }
