162f0fda54845f93b9cfb1cc09fc4b7caeef022d
Nawras GEORGI <support@nawrasg.fr>
2016-07-26 15:16:27 +0200

    First commit

---
+++ Setup/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name atlantisSetupApp
+ * @description
+ * # atlantisSetupApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('atlantisSetupApp', []);
---
+++ Setup/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name atlantisSetupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the atlantisSetupApp
+ */
+angular.module('atlantisSetupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
