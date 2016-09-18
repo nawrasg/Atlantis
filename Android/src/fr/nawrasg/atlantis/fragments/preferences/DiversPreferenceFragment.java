c393630b65896365c6a7238fd6839adf11e8d173
Nawras GEORGI <support@nawrasg.fr>
2016-09-18 23:37:51 +0200

    Add Dozer optimization option

--- Android/src/fr/nawrasg/atlantis/fragments/preferences/DiversPreferenceFragment.java
+++ Android/src/fr/nawrasg/atlantis/fragments/preferences/DiversPreferenceFragment.java
@@ -6,12 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
+import android.os.PowerManager;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -32,182 +36,218 @@ import fr.nawrasg.atlantis.R;
 import fr.nawrasg.atlantis.activities.SettingsActivity;
 import fr.nawrasg.atlantis.other.AtlantisContract;
 
-public class DiversPreferenceFragment extends PreferenceFragment implements OnPreferenceClickListener {
-	private Context mContext;
-	private Preference mImportPreference, mGcmPreference;
-	private EditTextPreference mSyncFrequencyPreference;
-	private GoogleCloudMessaging mGCM;
-	private String nRegId;
-	private String SENDER_ID = "926480811038";
-	
-	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-	public static final String PROPERTY_REG_ID = "registration_id";
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.pref_divers);
-		mContext = getActivity();
+public class DiversPreferenceFragment extends PreferenceFragment implements OnPreferenceClickListener, Preference.OnPreferenceChangeListener {
+    private Context mContext;
+    private Preference mImportPreference, mGcmPreference;
+    private SwitchPreference mDozePreference;
+    private EditTextPreference mSyncFrequencyPreference;
+    private GoogleCloudMessaging mGCM;
+    private String nRegId;
+    private String SENDER_ID = "926480811038";
+    private PowerManager mPM;
 
-		mSyncFrequencyPreference = (EditTextPreference) findPreference("sync_frequency");
-		mSyncFrequencyPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				Account nAccount = new Account(getString(R.string.app_name), AtlantisContract.AUTHORITY);
-				long nPeriod = Integer.parseInt(App.getString(mContext, "sync_frequency")) * 3600L;
-				if(nPeriod > 0){
-					ContentResolver.addPeriodicSync(nAccount, AtlantisContract.AUTHORITY, Bundle.EMPTY, nPeriod);
-					((SettingsActivity)getActivity()).launchSync();
-					return true;
-				}
-				return false;
-			}
-		});
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String PROPERTY_REG_ID = "registration_id";
 
-		mImportPreference = findPreference("import");
-		mImportPreference.setOnPreferenceClickListener(this);
-		
-		mGcmPreference = findPreference("gcm");
-		mGcmPreference.setOnPreferenceClickListener(this);
-		if(!App.getPrefString(mContext, PROPERTY_REG_ID).equals("")){
-			mGcmPreference.setSummary(getResources().getString(R.string.fragment_preference_divers_gcm_registered_device));
-		}
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_divers);
+        mContext = getActivity();
+        mPM = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mSyncFrequencyPreference = (EditTextPreference) findPreference("sync_frequency");
+        mSyncFrequencyPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Account nAccount = new Account(getString(R.string.app_name), AtlantisContract.AUTHORITY);
+                long nPeriod = Integer.parseInt(App.getString(mContext, "sync_frequency")) * 3600L;
+                if (nPeriod > 0) {
+                    ContentResolver.addPeriodicSync(nAccount, AtlantisContract.AUTHORITY, Bundle.EMPTY, nPeriod);
+                    ((SettingsActivity) getActivity()).launchSync();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-	@Override
-	public boolean onPreferenceClick(Preference preference) {
-		switch (preference.getKey()) {
-			case "import":
-				importSettings();
-				return true;
-			case "gcm":
-				GcmSubscription();
-				return true;
-		}
-		return false;
-	}
+        mImportPreference = findPreference("import");
+        mImportPreference.setOnPreferenceClickListener(this);
 
-	private void importSettings(){
-		String nURL = App.getFullUrl(mContext) + App.SETTINGS + "?api=" + App.getAPI(mContext) + "&type=Atlantis";
-		Request nRequest = new Request.Builder().url(nURL).build();
-		App.httpClient.newCall(nRequest).enqueue(new Callback() {
-			@Override
-			public void onFailure(Request request, IOException e) {
+        mGcmPreference = findPreference("gcm");
+        mGcmPreference.setOnPreferenceClickListener(this);
+        if (!App.getPrefString(mContext, PROPERTY_REG_ID).equals("")) {
+            mGcmPreference.setSummary(getResources().getString(R.string.fragment_preference_divers_gcm_registered_device));
+        }
 
-			}
+        mDozePreference = (SwitchPreference) findPreference("doze");
 
-			@Override
-			public void onResponse(Response response) throws IOException {
-				try {
-					JSONObject nJson = new JSONObject(response.body().string());
-					if (nJson.getString("name").equals("Atlantis")) {
-						importSettings(nJson);
-					} else {
-						Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_settings_import_fail), Toast.LENGTH_LONG).show();
-					}
-				} catch (Exception e) {
-					Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
-				}
-			}
-		});
-	}
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == App.CODE_PLAN) {
-			setPlanPath(data.getData());
-		}
-	}
-	
-	private void GcmSubscription(){
-		if (checkPlayServices()) {
-			mGCM = GoogleCloudMessaging.getInstance(mContext);
-			nRegId = getRegistrationId();
-			registerGCM();
-			Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registration_done), Toast.LENGTH_SHORT).show();
-		} else {
-			Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_not_available), Toast.LENGTH_SHORT).show();
-		}
-	}
-	
-	private String getRegistrationId() {
-		String registrationId = App.getString(mContext, PROPERTY_REG_ID);
-		if (registrationId.isEmpty()) {
-			Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registration_progress), Toast.LENGTH_SHORT).show();
-			return "";
-		}
-		Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registered_device), Toast.LENGTH_SHORT).show();
-		return registrationId;
-	}
-	
-	private boolean checkPlayServices() {
-		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(mContext);
-		if (resultCode != ConnectionResult.SUCCESS) {
-			if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-				GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(), PLAY_SERVICES_RESOLUTION_REQUEST).show();
-			} else {
-				Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_not_available), Toast.LENGTH_SHORT).show();
-			}
-			return false;
-		}
-		return true;
-	}
+    }
 
-	public void setPlanPath(Uri uri) {
-		App.setString(mContext, "plan", uri.toString());
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (mPM.isIgnoringBatteryOptimizations(mContext.getPackageName())) {
+                mDozePreference.setChecked(true);
+            } else {
+                mDozePreference.setChecked(false);
+            }
+            mDozePreference.setOnPreferenceChangeListener(this);
+        } else {
+            mDozePreference.setEnabled(false);
+        }
+    }
 
-	private void importSettings(JSONObject json) {
-		try {
-			App.setString(mContext, "urlExterne", json.getString("url"));
-			//App.setString(mContext, "wifiSSID", json.getString("ssid"));
-			//App.setString(mContext, "wifiBSSID", json.getString("mac"));
-			App.setFloat(mContext, "homeRadius", (float) json.getDouble("radius"));
-			App.setFloat(mContext, "homeLong", (float) json.getDouble("long"));
-			App.setFloat(mContext, "homeLat", (float) json.getDouble("lat"));
-			Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_settings_import_success), Toast.LENGTH_SHORT).show();
-		} catch (Exception e) {
-			Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
-		}
-	}
-	
-	private void registerGCM() {
-		new AsyncTask<Void, Void, String>() {
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        switch (preference.getKey()) {
+            case "import":
+                importSettings();
+                return true;
+            case "gcm":
+                GcmSubscription();
+                return true;
+        }
+        return false;
+    }
 
-			@Override
-			protected String doInBackground(Void... params) {
-				try {
-					if (mGCM == null) {
-						mGCM = GoogleCloudMessaging.getInstance(mContext);
-					}
-					nRegId = mGCM.register(SENDER_ID);
-					App.setString(mContext, PROPERTY_REG_ID, nRegId);
-					Looper.prepare();
-					regGCM(nRegId);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				return null;
-			}
+    private void importSettings() {
+        String nURL = App.getFullUrl(mContext) + App.SETTINGS + "?api=" + App.getAPI(mContext) + "&type=Atlantis";
+        Request nRequest = new Request.Builder().url(nURL).build();
+        App.httpClient.newCall(nRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
 
-		}.execute(null, null, null);
-	}
+            }
 
-	private void regGCM(String data){
-		String nURL = App.getFullUrl(mContext) + App.GCM + "?api=" + App.getAPI(mContext) + "&gcm=" + data;
-		Request nRequest = new Request.Builder().url(nURL).post(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), "")).build();
-		App.httpClient.newCall(nRequest).enqueue(new Callback() {
-			@Override
-			public void onFailure(Request request, IOException e) {
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    JSONObject nJson = new JSONObject(response.body().string());
+                    if (nJson.getString("name").equals("Atlantis")) {
+                        importSettings(nJson);
+                    } else {
+                        Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_settings_import_fail), Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(mContext, e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
-			}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == App.CODE_PLAN) {
+            setPlanPath(data.getData());
+        }
+    }
 
-			@Override
-			public void onResponse(Response response) throws IOException {
+    private void GcmSubscription() {
+        if (checkPlayServices()) {
+            mGCM = GoogleCloudMessaging.getInstance(mContext);
+            nRegId = getRegistrationId();
+            registerGCM();
+            Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registration_done), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_not_available), Toast.LENGTH_SHORT).show();
+        }
+    }
 
-			}
-		});
-	}
+    private String getRegistrationId() {
+        String registrationId = App.getString(mContext, PROPERTY_REG_ID);
+        if (registrationId.isEmpty()) {
+            Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registration_progress), Toast.LENGTH_SHORT).show();
+            return "";
+        }
+        Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_registered_device), Toast.LENGTH_SHORT).show();
+        return registrationId;
+    }
 
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(mContext);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(), PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_gcm_not_available), Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void setPlanPath(Uri uri) {
+        App.setString(mContext, "plan", uri.toString());
+    }
+
+    private void importSettings(JSONObject json) {
+        try {
+            App.setString(mContext, "urlExterne", json.getString("url"));
+            //App.setString(mContext, "wifiSSID", json.getString("ssid"));
+            //App.setString(mContext, "wifiBSSID", json.getString("mac"));
+            App.setFloat(mContext, "homeRadius", (float) json.getDouble("radius"));
+            App.setFloat(mContext, "homeLong", (float) json.getDouble("long"));
+            App.setFloat(mContext, "homeLat", (float) json.getDouble("lat"));
+            Toast.makeText(mContext, getResources().getString(R.string.fragment_preference_divers_settings_import_success), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void registerGCM() {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... params) {
+                try {
+                    if (mGCM == null) {
+                        mGCM = GoogleCloudMessaging.getInstance(mContext);
+                    }
+                    nRegId = mGCM.register(SENDER_ID);
+                    App.setString(mContext, PROPERTY_REG_ID, nRegId);
+                    Looper.prepare();
+                    regGCM(nRegId);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+        }.execute(null, null, null);
+    }
+
+    private void regGCM(String data) {
+        String nURL = App.getFullUrl(mContext) + App.GCM + "?api=" + App.getAPI(mContext) + "&gcm=" + data;
+        Request nRequest = new Request.Builder().url(nURL).post(RequestBody.create(MediaType.parse("text/x-markdown; charset=utf-8"), "")).build();
+        App.httpClient.newCall(nRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+            }
+        });
+    }
+
+    private void ignoreDoze() {
+        Intent nIntent = new Intent();
+        nIntent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+        mContext.startActivity(nIntent);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case "doze":
+                ignoreDoze();
+                return true;
+        }
+        return false;
+    }
 }
