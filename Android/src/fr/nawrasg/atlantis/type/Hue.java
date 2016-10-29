1e58a4e8a0b2743eda36e0e723c486d0a4c35eee
Nawras GEORGI <support@nawrasg.fr>
2016-10-29 21:45:06 +0200

    Format code

--- Android/src/fr/nawrasg/atlantis/type/Hue.java
+++ Android/src/fr/nawrasg/atlantis/type/Hue.java
@@ -1,98 +1,98 @@
 package fr.nawrasg.atlantis.type;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.database.Cursor;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import fr.nawrasg.atlantis.other.AtlantisContract;
 
-public class Hue extends Light{
-	private String mUID, mBrightness, mReachable, mOn;
-	
-	public Hue(JSONObject json) {
-		super(json);
-		try{
-			mUID = json.getString(AtlantisContract.Lights.COLUMN_UID);
-			mReachable = json.getString(AtlantisContract.Lights.COLUMN_REACHABLE);
-			mOn = json.getString(AtlantisContract.Lights.COLUMN_ON);
-			mBrightness = json.getString(AtlantisContract.Lights.COLUMN_BRIGHTNESS);
-		}catch(JSONException e){
-			Log.w("Atlantis", e.toString());
-		}
-	}
+public class Hue extends Light {
+    private String mUID, mBrightness, mReachable, mOn;
 
-	public Hue(Cursor cursor){
-		super(cursor);
-		mUID = cursor.getString(cursor.getColumnIndex(AtlantisContract.Lights.COLUMN_UID));
-		mBrightness = "0";
-		mReachable = "false";
-		mOn = "false";
-	}
-	
-	public Hue(Parcel in){
-		super(in);
-		mUID = in.readString();
-		mReachable = in.readString();
-		mOn = in.readString();
-		mBrightness = in.readString();
-	}
-	
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		super.writeToParcel(dest, flags);
-		dest.writeString(mUID);
-		dest.writeString(mReachable);
-		dest.writeString(mOn);
-		dest.writeString(mBrightness);
-	}
-	
-	public int getBrightness(){
-		return Integer.parseInt(mBrightness);
-	}
+    public Hue(JSONObject json) {
+        super(json);
+        try {
+            mUID = json.getString(AtlantisContract.Lights.COLUMN_UID);
+            mReachable = json.getString(AtlantisContract.Lights.COLUMN_REACHABLE);
+            mOn = json.getString(AtlantisContract.Lights.COLUMN_ON);
+            mBrightness = json.getString(AtlantisContract.Lights.COLUMN_BRIGHTNESS);
+        } catch (JSONException e) {
+            Log.w("Atlantis", e.toString());
+        }
+    }
 
-	public void setBrightness(int brightness){
-		mBrightness = brightness + "";
-	}
-	
-	public boolean isReachable(){
-		return Boolean.parseBoolean(mReachable);
-	}
+    public Hue(Cursor cursor) {
+        super(cursor);
+        mUID = cursor.getString(cursor.getColumnIndex(AtlantisContract.Lights.COLUMN_UID));
+        mBrightness = "0";
+        mReachable = "false";
+        mOn = "false";
+    }
 
-	public void setReachable(boolean reachable){
-		mReachable = reachable + "";
-	}
-	
-	public boolean isOn(){
-		return Boolean.parseBoolean(mOn);
-	}
+    public Hue(Parcel in) {
+        super(in);
+        mUID = in.readString();
+        mReachable = in.readString();
+        mOn = in.readString();
+        mBrightness = in.readString();
+    }
 
-	public void setOn(boolean on){
-		mOn = on + "";
-	}
-	
-	public String getUID(){
-		return mUID;
-	}
-	
-	public static final Parcelable.Creator<Hue> CREATOR = new Parcelable.Creator<Hue>() {
-		@Override
-		public Hue createFromParcel(Parcel source) {
-			return new Hue(source);
-		}
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(mUID);
+        dest.writeString(mReachable);
+        dest.writeString(mOn);
+        dest.writeString(mBrightness);
+    }
 
-		@Override
-		public Hue[] newArray(int size) {
-			return new Hue[size];
-		}
-	};
+    public int getBrightness() {
+        return Integer.parseInt(mBrightness);
+    }
 
-	public void update(Hue hue){
-		setReachable(hue.isReachable());
-		setBrightness(hue.getBrightness());
-		setOn(hue.isOn());
-	}
+    public void setBrightness(int brightness) {
+        mBrightness = brightness + "";
+    }
+
+    public boolean isReachable() {
+        return Boolean.parseBoolean(mReachable);
+    }
+
+    public void setReachable(boolean reachable) {
+        mReachable = reachable + "";
+    }
+
+    public boolean isOn() {
+        return Boolean.parseBoolean(mOn);
+    }
+
+    public void setOn(boolean on) {
+        mOn = on + "";
+    }
+
+    public String getUID() {
+        return mUID;
+    }
+
+    public static final Parcelable.Creator<Hue> CREATOR = new Parcelable.Creator<Hue>() {
+        @Override
+        public Hue createFromParcel(Parcel source) {
+            return new Hue(source);
+        }
+
+        @Override
+        public Hue[] newArray(int size) {
+            return new Hue[size];
+        }
+    };
+
+    public void update(Hue hue) {
+        setReachable(hue.isReachable());
+        setBrightness(hue.getBrightness());
+        setOn(hue.isOn());
+    }
 }
