58c0c51a54de0159d010fbca726b641c6f012441
Nawras GEORGI <support@nawrasg.fr>
2016-11-04 22:21:35 +0100

    Remove getMap()

--- Android/src/fr/nawrasg/atlantis/type/Element.java
+++ Android/src/fr/nawrasg/atlantis/type/Element.java
@@ -1,66 +1,57 @@
 package fr.nawrasg.atlantis.type;
 
-import java.util.HashMap;
-
 import org.json.JSONObject;
 
 public class Element {
-	private String mName;
-	private int mQuantity, mID;
-	private boolean mDone;
-	
-	public Element(JSONObject x){
-		try{
-			mID = x.getInt("id");
-			mName = x.getString("name");
-			mQuantity = x.getInt("quantity");
-			mDone = false;
-		}catch(Exception e){
-			
-		}
-	}
+    private String mName;
+    private int mQuantity, mID;
+    private boolean mDone;
 
-	public Element(String name, String quantity){
-		try{
-			mName = name;
-			mQuantity = Integer.parseInt(quantity);
-		}catch(Exception e){
+    public Element(JSONObject x) {
+        try {
+            mID = x.getInt("id");
+            mName = x.getString("name");
+            mQuantity = x.getInt("quantity");
+            mDone = false;
+        } catch (Exception e) {
 
-		}
-	}
-	
-	public HashMap<String, String> getMap(){
-		HashMap<String, String> nMap = new HashMap<String, String>();
-		nMap.put("titre", mName);
-		nMap.put("quantite", mQuantity + "");
-		return nMap;
-	}
-	
-	public int getQuantity(){
-		return mQuantity;
-	}
-	
-	public String getName(){
-		return mName;
-	}
-	
-	public int getID(){
-		return mID;
-	}
+        }
+    }
 
-	public void increment(){
-		mQuantity++;
-	}
+    public Element(String name, String quantity) {
+        try {
+            mName = name;
+            mQuantity = Integer.parseInt(quantity);
+        } catch (Exception e) {
 
-	public void decrement(){
-		mQuantity--;
-	}
+        }
+    }
 
-	public boolean isDone(){
-		return mDone;
-	}
+    public int getQuantity() {
+        return mQuantity;
+    }
 
-	public void done(boolean value){
-		mDone = value;
-	}
+    public String getName() {
+        return mName;
+    }
+
+    public int getID() {
+        return mID;
+    }
+
+    public void increment() {
+        mQuantity++;
+    }
+
+    public void decrement() {
+        mQuantity--;
+    }
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void done(boolean value) {
+        mDone = value;
+    }
 }
